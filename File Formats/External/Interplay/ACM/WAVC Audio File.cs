8c4532f253a80d84b53d86e1223cafbac19569c7
Erik "Bardez" Frantz <bardez@bardez.net>
2011-11-12 22:01:22 +0000

    Added in WAVC format code
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C81

---
+++ File Formats/External/Interplay/ACM/WAVC Audio File.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+
+using Bardez.Projects.ReusableCode;
+using Bardez.Projects.Win32.Audio;
+
+namespace Bardez.Projects.InfinityPlus1.Files.External.Interplay.ACM
+{
+    /// <summary>Represents an ACM file with a WAVC header</summary>
+    /// <remarks>This is literally just a WAVC header prepended to an ACM file stream; its ACM header exists after the WAVC header</remarks>
+    public class WavCAudioFile : AcmAudioFile
+    {
+        /// <summary>WAVC Header information</summary>
+        public WavCHeader WavcHeader { get; set; }
+        
+        #region Construction
+        /// <summary>Default constructor</summary>
+        public WavCAudioFile() : base()
+        {
+            this.WavcHeader = null;
+        }
+
+        /// <summary>Initializes field data.</summary>
+        protected override void Initialize()
+        {
+            this.WavcHeader = new WavCHeader();
+            base.Initialize();
+        }
+        #endregion
+
+        #region IO
+        /// <summary>This public method reads file format from the input stream. Reads the whole structure.</summary>
+        /// <param name="input">Input stream to read from</param>
+        /// <param name="initialize">Boolean indicating whether or not to initialize the field data</param>
+        public override void Read(Stream input, Boolean initialize = true)
+        {
+            //(re-)Instatiate fields
+            if (initialize)
+                this.Initialize();
+
+            this.WavcHeader.Read(input);
+            ReusableIO.SeekIfAble(input, this.WavcHeader.OffsetAcm);
+            base.Read(input, false);
+        }
+
+        /// <summary>This public method reads file format from the input stream. Reads the whole structure.</summary>
+        /// <param name="input">Input stream to read from</param>
+        public override void ReadHeader(Stream input)
+        {
+            this.Initialize();
+            this.WavcHeader.Read(input);
+            ReusableIO.SeekIfAble(input, this.WavcHeader.OffsetAcm);
+            base.ReadHeader(input);
+        }
+        #endregion
+
+        /// <summary>Returns a WaveFormatEx instance from this header data</summary>
+        /// <returns>A WaveFormatEx instance to submit to API calls</returns>
+        public override WaveFormatEx GetWaveFormat()
+        {
+            WaveFormatEx waveEx = new WaveFormatEx();
+
+            waveEx.AverageBytesPerSec = this.AcmHeader.SampleRate * 2U /*sizeof(short)*/ * this.WavcHeader.Channels /* sizeof(usort) */;
+            waveEx.BitsPerSample = 16; /* sizeof(short) */
+            waveEx.BlockAlignment = Convert.ToUInt16(2U * this.WavcHeader.Channels);
+            waveEx.FormatTag = 1;   //1 for PCM
+            waveEx.NumberChannels = this.WavcHeader.Channels; //designating 1 causes errors
+            waveEx.SamplesPerSec = this.AcmHeader.SampleRate;
+            waveEx.Size = 0;    //no extra data; this is strictly a WaveFormatEx instance 
+
+            return waveEx;
+        }
+    }
+}
---
+++ File Formats/External/Interplay/ACM/WAVC Header.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using Bardez.Projects.InfinityPlus1.Files.Infinity.Globals;
+using Bardez.Projects.ReusableCode;
+
+namespace Bardez.Projects.InfinityPlus1.Files.External.Interplay.ACM
+{
+    public class WavCHeader
+    {
+        #region Fields & Members
+        /// <summary>File signature</summary>
+        public String Signature { get; set; }
+
+        /// <summary>File version</summary>
+        public String Version { get; set; }
+
+        /// <summary>Uncompressed size of the ACM file</summary>
+        /// <remarks>Is this raw PCM bytes, or inclusive of header?</remarks>
+        public UInt32 SizeUncompressed { get; set; }
+
+        /// <summary>Compresed ACM file dize</summary>
+        public UInt32 SizeCompressed { get; set; }
+
+        /// <summary>Offset to theACM file</summary>
+        public UInt32 OffsetAcm { get; set; }
+
+        /// <summary>Count of channels</summary>
+        /// <remarks>This channel countis actualy trustworthy, as opposed to actual ACM</remarks>
+        public UInt16 Channels { get; set; }
+
+        /// <summary>Count of bits per sample</summary>
+        /// <value>Usually 16</value>
+        public UInt16 BitsPerSample { get; set; }
+
+        /// <summary>Number of samples per second</summary>
+        public UInt32 Frequency { get; set; }
+        #endregion
+        
+        #region Construction
+        /// <summary>Default constructor</summary>
+        public WavCHeader() { }
+
+        /// <summary>Definition constructor</summary>
+        /// <param name="signature">Signature string, usually "WAVC"</param>
+        /// <param name="version">Version string, usually "V1.0"</param>
+        /// <param name="uncompressedSize">Raw PCM sample size in bytes</param>
+        /// <param name="compressedSize">Compressed ACM file size</param>
+        /// <param name="acmOffset">Offset frm start of file to ACM subfile</param>
+        /// <param name="channels">Reliable count of channels</param>
+        /// <param name="sampleBitSize">Cont of bits per sample</param>
+        /// <param name="frequency">Number of samples per second</param>
+        public WavCHeader(String signature, String version, UInt32 uncompressedSize, UInt32 compressedSize, UInt32 acmOffset,  UInt16 channels, UInt16 sampleBitSize, UInt16 frequency)
+        {
+            this.Signature = signature;
+            this.Version = version;
+            this.SizeUncompressed = uncompressedSize;
+            this.SizeCompressed = compressedSize;
+            this.OffsetAcm = acmOffset;
+            this.Channels = channels;
+            this.BitsPerSample = sampleBitSize;
+            this.Frequency = frequency;
+        }
+        #endregion
+
+        #region IO
+        /// <summary>Read method, reads ACM header from the input stream</summary>
+        /// <param name="input">Stream to read the header from</param>
+        public void Read(Stream input)
+        {
+            Byte[] header = ReusableIO.BinaryRead(input, 28);
+
+            this.Signature = ReusableIO.ReadStringFromByteArray(header, 0, Constants.CultureCodeEnglish, 4);
+            this.Version = ReusableIO.ReadStringFromByteArray(header, 4, Constants.CultureCodeEnglish, 4);
+            this.SizeUncompressed = ReusableIO.ReadUInt32FromArray(header, 8);
+            this.SizeCompressed = ReusableIO.ReadUInt32FromArray(header, 12);
+            this.OffsetAcm = ReusableIO.ReadUInt32FromArray(header, 16);
+            this.Channels = ReusableIO.ReadUInt16FromArray(header, 20);
+            this.BitsPerSample = ReusableIO.ReadUInt16FromArray(header, 22);
+            this.Frequency = ReusableIO.ReadUInt32FromArray(header, 24);
+        }
+
+        /// <summary>Write method, writes the ACM header to the output stream</summary>
+        /// <param name="output">Stream to write the header to</param>
+        public void Write(Stream output)
+        {
+            ReusableIO.WriteStringToStream(this.Signature, output, Constants.CultureCodeEnglish, false, 4);
+            ReusableIO.WriteStringToStream(this.Version, output, Constants.CultureCodeEnglish, false, 4);
+            ReusableIO.WriteUInt32ToStream(this.SizeUncompressed, output);
+            ReusableIO.WriteUInt32ToStream(this.SizeCompressed, output);
+            ReusableIO.WriteUInt32ToStream(this.OffsetAcm, output);
+            ReusableIO.WriteUInt16ToStream(this.Channels, output);
+            ReusableIO.WriteUInt16ToStream(this.BitsPerSample, output);
+            ReusableIO.WriteUInt32ToStream(this.Frequency, output);
+        }
+        #endregion
+
+        #region ToString
+        /// <summary>Overridden ToString() implementation</summary>
+        /// <returns>A String representation of this WAVC Header</returns>
+        public override String ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.AppendLine("Interplay ACM header:");
+            builder.Append(StringFormat.ToStringAlignment("Signature"));
+            builder.Append(this.Signature);
+            builder.Append(StringFormat.ToStringAlignment("Version"));
+            builder.Append(this.Version);
+            builder.Append(StringFormat.ToStringAlignment("Size (Uncompressed)"));
+            builder.Append(this.SizeUncompressed);
+            builder.Append(StringFormat.ToStringAlignment("Size (Compressed)"));
+            builder.Append(this.SizeCompressed);
+            builder.Append(StringFormat.ToStringAlignment("ACM Offset"));
+            builder.Append(this.OffsetAcm);
+            builder.Append(StringFormat.ToStringAlignment("Channels"));
+            builder.Append(this.Channels);
+            builder.Append(StringFormat.ToStringAlignment("Sample size (bits)"));
+            builder.Append(this.BitsPerSample);
+            builder.Append(StringFormat.ToStringAlignment("Sample rate"));
+            builder.Append(this.Frequency);
+            builder.Append("\n\n");
+
+            return builder.ToString();
+        }
+        #endregion
+    }
+}
