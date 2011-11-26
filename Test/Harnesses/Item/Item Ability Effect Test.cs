cb7700317b40ebe1d3656323f9781ddcfd6ffd2d
Erik "Bardez" Frantz <bardez@bardez.net>
2011-11-26 05:00:57 +0000

    Updated test class
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C108

--- Test/Harnesses/Item/Item Ability Effect Test.cs
+++ Test/Harnesses/Item/Item Ability Effect Test.cs
@@ -2,56 +2,76 @@
 using System.IO;
 
 using Bardez.Projects.Configuration;
-using Bardez.Projects.InfinityPlus1.Files.Infinity.Common.ItmSpl;
-using Bardez.Projects.InfinityPlus1.Files.Infinity.Effect;
 using Bardez.Projects.InfinityPlus1.Files.Infinity.Effect.Effect1;
-using Bardez.Projects.InfinityPlus1.Files.Infinity.Item;
 using Bardez.Projects.InfinityPlus1.Files.Infinity.Item.Item1;
 using Bardez.Projects.InfinityPlus1.Test;
-using Bardez.Projects.InfinityPlus1.Utility.UiInterceptor;
+using Bardez.Projects.ReusableCode;
 
-namespace Bardez.Projects.InfinityPlus1.Test.Item
+namespace Bardez.Projects.InfinityPlus1.Test.Harnesses.Item
 {
     /// <summary>This class tests the usable methods in the Bardez.Projects.InfinityPlus1.Files.Infinity.Common.ItmSpl.ItemSpellAbilityEffect class.</summary>
-    public class ItemSpellAbilityEffectTest : ITester
+    public class ItemSpellAbilityEffectTest : FileTesterBase
     {
-        protected ItemHeader1 header;
-        protected Effect1 effect;
+        #region Fields
+        /// <summary>Constant key to look up in app.config</summary>
+        protected const String configKey = "Test.Item.Item1Path";
 
-        #region Properties
-        public ItemHeader1 Header
-        {
-            get { return this.header; }
-        }
+        /// <summary>Instance supporting data read</summary>
+        protected ItemHeader1 Header { get; set; }
 
-        public Effect1 Effect
+        /// <summary>Format instance to test</summary>
+        protected Effect1 Effect { get; set; }
+        #endregion
+
+        #region Construction
+        /// <summary>Default constructor</summary>
+        public ItemSpellAbilityEffectTest()
         {
-            get { return this.effect; }
+            this.InitializeInstance();
         }
         #endregion
 
-        public void Test()
+        /// <summary>Initializes the test class data</summary>
+        /// <param name="sender">Object sending/raising the request</param>
+        /// <param name="e">Specific initialization event parameters</param>
+        protected override void InitializeTestData(Object sender, EventArgs e)
         {
-            String path = ConfigurationHandler.GetSettingValue("Test.Item.Item1Path");
-            this.Test(path);
+            this.FilePaths = ConfigurationHandlerMulti.GetSettingValues(ItemSpellAbilityEffectTest.configKey);
         }
 
-        public void Test(String Path)
+        /// <summary>Event to raise for testing instance(s)</summary>
+        /// <param name="sender">Object sending/raising the request</param>
+        /// <param name="testArgs">Arguments containing the item to test (usually a file path)</param>
+        protected override void TestCase(Object sender, TestEventArgs testArgs)
         {
-            using (FileStream stream = new FileStream(Path, FileMode.Open, FileAccess.Read))
-                this.Test(stream);
+            String effect = "No abilities";
+            using (FileStream stream = new FileStream(testArgs.Path, FileMode.Open, FileAccess.Read))
+            {
+                this.Header = new ItemHeader1();
+                this.Header.Read(stream);
+
+                if (this.Header.CountAbilities > 0)
+                {
+                    ReusableIO.SeekIfAble(stream, this.Header.OffsetAbilityEffects);
+                    this.Effect = new Effect1();
+                    this.Effect.Read(stream);
+                    effect = this.Effect.ToString();
+                }
+            }
+
+            this.DoPostMessage(new MessageEventArgs(effect));
+
+            if (this.Header.CountAbilities > 0)
+                //not sure how I want to handle this...
+                using (FileStream dest = new FileStream(testArgs.Path + ".abilityeffect.rewrite", FileMode.Create, FileAccess.Write))
+                    this.TestWrite(dest);
         }
 
-        public void Test(Stream Source)
+        /// <summary>Writes the data structure back out to a destination stream</summary>
+        /// <param name="destination">Stream to write output to</param>
+        protected virtual void TestWrite(Stream destination)
         {
-            this.header = new ItemHeader1();
-            this.header.Read(Source);
-
-            Source.Seek(this.header.OffsetAbilityEffects, SeekOrigin.Begin);
-            this.effect = new Effect1();
-            this.effect.Read(Source);
-
-            Interceptor.WriteMessage(this.effect.ToString());
+            this.Effect.Write(destination);
         }
     }
 }
