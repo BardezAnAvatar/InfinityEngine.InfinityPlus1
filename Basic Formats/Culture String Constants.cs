c5d5fc19f9c73eefd5f92e5f8423dbf8da1861d5
Erik "Bardez" Frantz <bardez@bardez.net>
2012-01-12 02:13:42 +0000

    Made publically visible
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C137

--- Basic Formats/Culture String Constants.cs
+++ Basic Formats/Culture String Constants.cs
@@ -3,18 +3,18 @@
 namespace Bardez.Projects.InfinityPlus1.FileFormats.Base
 {
     /// <summary>Contains a list of constants</summary>
-    internal static class CultureConstants
+    public static class CultureConstants
     {
         /// <summary>String representing the English localisation code</summary>
-        internal const String CultureCodeEnglish = "en-US";
+        public const String CultureCodeEnglish = "en-US";
 
         /// <summary>String representing the French localisation code</summary>
-        internal const String CultureCodeFrench = "fr";
+        public const String CultureCodeFrench = "fr";
 
         /// <summary>String representing the German localisation code</summary>
-        internal const String CultureCodeGerman = "de";
+        public const String CultureCodeGerman = "de";
 
         /// <summary>String representing the Spanish localisation code</summary>
-        internal const String CultureCodeSpanish = "es";
+        public const String CultureCodeSpanish = "es";
     }
 }
