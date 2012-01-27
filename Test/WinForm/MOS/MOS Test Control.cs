df0f20fc04a822da28d0dc81b04a0a10daa1bc21
Erik "Bardez" Frantz <bardez@bardez.net>
2012-01-27 06:20:52 +0000

    Added in harness teting controls
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C181

---
+++ Test/WinForm/MOS/MOS Test Control.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Bardez.Projects.InfinityPlus1.Test.Harnesses.MapOfScreen;
+
+namespace Bardez.Projects.InfinityPlus1.Test.WinForm.DLG
+{
+    /// <summary>User control for testing the MOS file class</summary>
+    public class MosTestControl : HarnessFileBaseTestControlBase<MosTest>
+    {
+        /// <summary>Default constructor</summary>
+        public MosTestControl()
+        {
+            this.InitializeComponent();
+            this.Harness = new MosTest();
+            this.InitializeControlFields();
+        }
+    }
+}
