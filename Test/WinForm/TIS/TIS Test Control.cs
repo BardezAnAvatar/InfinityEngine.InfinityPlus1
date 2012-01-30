da4c4d460bf3f6e42b7b5279b2b82e19dbc7bf7d
Erik "Bardez" Frantz <bardez@bardez.net>
2012-01-30 00:25:46 +0000

    Added in TIS header test controls
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C203

---
+++ Test/WinForm/TIS/TIS Test Control.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Bardez.Projects.InfinityPlus1.Test.Harnesses.TileSet;
+
+namespace Bardez.Projects.InfinityPlus1.Test.WinForm.TIS
+{
+    /// <summary>User control for testing the MOS file class</summary>
+    public class TisTestControl : HarnessFileBaseTestControlBase<TisHeaderTest>
+    {
+        /// <summary>Default constructor</summary>
+        public TisTestControl()
+        {
+            this.InitializeComponent();
+            this.Harness = new TisHeaderTest();
+            this.InitializeControlFields();
+        }
+    }
+}
