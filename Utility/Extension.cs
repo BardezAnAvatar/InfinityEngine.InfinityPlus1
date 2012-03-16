6048a103a71e0129a3fe9dc7ffcc32dc7dd58c80
Erik "Bardez" Frantz <bardez@bardez.net>
2012-03-16 23:01:49 +0000

    Changed the List extension methods to IList methods, added in a hashing generic
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C235

--- Utility/Extension.cs
+++ Utility/Extension.cs
@@ -9,7 +9,7 @@ namespace Bardez.Projects.InfinityPlus1.Utility
         /// <typeparam name="T">Type of object that is a class and implements IDeepCloneable.</typeparam>
         /// <param name="reference">List to clone.</param>
         /// <returns>A deeply cloned list.</returns>
-        public static List<T> Clone<T>(this List<T> reference) where T : class, IDeepCloneable
+        public static List<T> Clone<T>(this IList<T> reference) where T : class, IDeepCloneable
         {
             List<T> clone = new List<T>();
             foreach (T type in reference)
@@ -25,7 +25,7 @@ namespace Bardez.Projects.InfinityPlus1.Utility
         /// <param name="reference">Left "this" compared List</param>
         /// <param name="compare">Right compared List</param>
         /// <returns>Boolean indicating equality</returns>
-        public static Boolean Equals<T>(this List<T> reference, List<T> compare)
+        public static Boolean Equals<T>(this IList<T> reference, IList<T> compare)
         {
             Boolean equal = false;
 
@@ -45,6 +45,21 @@ namespace Bardez.Projects.InfinityPlus1.Utility
             catch { equal = false; }    //per MSDN, must not throw exceptions
 
             return equal;
+        }
+
+        /// <summary>Extension method that performs GetHashCode on all list elements</summary>
+        /// <typeparam name="T">Generic List type</typeparam>
+        /// <param name="reference">"this" List instance</param>
+        /// <returns>The computed hash code</returns>
+        public static Int32 GetHashCode<T>(this IList<T> reference)
+        {
+            Int32 hashcode = 0;
+
+            if (reference != null)
+                foreach (T item in reference)
+                    hashcode ^= item.GetHashCode();
+
+            return hashcode;
         }
     }
 }
