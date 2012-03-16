340a6723c1f6b1c155e22dd5fca7c4bf0b685f43
Erik "Bardez" Frantz <bardez@bardez.net>
2012-03-16 23:35:34 +0000

    Added in Audio testing support filed
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C253

---
+++ Test/Harnesses/Audio Test Event Arguments.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Bardez.Projects.InfinityPlus1.Test.Harnesses
+{
+    /// <summary>Event Arguments containing a message to post</summary>
+    public class AudioTestEventArgs : TestEventArgs
+    {
+        #region
+        /// <summary>Channel number to test</summary>
+        public Int32 Channel { get; set; }
+        #endregion
+
+        /// <summary>Default constructor</summary>
+        public AudioTestEventArgs() : base()
+        {
+            this.Path = null;
+        }
+
+        /// <summary>Definition constructor</summary>
+        /// <param name="path">Path to test</param>
+        public AudioTestEventArgs(String path, Int32 channelNumber) : base(path)
+        {
+            this.Channel = channelNumber;
+        }
+    }
+}
---
+++ Test/Harnesses/IAudioTester.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bardez.Projects.InfinityPlus1.Test.Harnesses
+{
+    public interface IAudioTester
+    {
+        /// <summary>Method exposing a stop command</summary>
+        void StopPlayback();
+    }
+}
