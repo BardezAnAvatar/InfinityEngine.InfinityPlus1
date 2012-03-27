8626d1fccccc5b75dbd806a98b51cf68821a867a
Erik "Bardez" Frantz <bardez@bardez.net>
2012-03-27 00:35:06 +0000

    Added in constructor and a StartFrame parameter so the playback engine might know when to start audio playback
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C295

--- Infinity Formats/External/Interplay/MVE/Component/Management/MVE Audio Channel Parameters.cs
+++ Infinity Formats/External/Interplay/MVE/Component/Management/MVE Audio Channel Parameters.cs
@@ -2,8 +2,10 @@
 
 namespace Bardez.Projects.InfinityPlus1.FileFormats.External.Interplay.MVE.Component.Management
 {
+    /// <summary>This class holds basic Audio parameters for an MVE file</summary>
     public class MveAudioChannelParams
     {
+        #region Fields
         /// <summary>Exposes the number of channels in the audio stream</summary>
         public Int32 Channels { get; set; }
 
@@ -15,5 +17,18 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.External.Interplay.MVE.Compo
 
         /// <summary>Represents  the sample rate of the audio buffer</summary>
         public Int32 SampleRate { get; set; }
+
+        /// <summary>Represents the chunk number in which the opcode to Initialize Audio buffers fisr appears</summary>
+        public Int32 StartFrame { get; set; }
+        #endregion
+
+
+        #region Construction
+        /// <summary>Default constructor</summary>
+        public MveAudioChannelParams()
+        {
+            this.StartFrame = -1;
+        }
+        #endregion
     }
 }
