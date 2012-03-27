a6600c1190110caddd72d0830fb630d8c25041f2
Erik "Bardez" Frantz <bardez@bardez.net>
2012-03-27 00:31:21 +0000

    Irrelevant changes; code is not used
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C288

--- Infinity Formats/External/Interplay/MVE/Component/Opcodes/Toggle Audio.cs
+++ Infinity Formats/External/Interplay/MVE/Component/Opcodes/Toggle Audio.cs
@@ -1,17 +1,18 @@
 ﻿using System;
+using System.IO;
 
 using Bardez.Projects.InfinityPlus1.FileFormats.External.Interplay.MVE.Enum;
 
 namespace Bardez.Projects.InfinityPlus1.FileFormats.External.Interplay.MVE.Component.Opcodes
 {
-    /// <summary>Represents an end-of-stream opcode to cease all decoding</summary>
+    /// <summary>Represents an opcode to cease toggle audio playback</summary>
     public class ToggleAudio : OpcodeData
     {
-        #region Properties
-        /// <summary>Exposes the opcode enum for this opcode</summary>
-        public override OpcodeTypes Opcode
+        #region IO method implemetations
+        /// <summary>This public method writes the file format to the output stream.</summary>
+        /// <param name="output">Stream to write to</param>
+        public override void Write(Stream output)
         {
-            get { return OpcodeTypes.ToggleAudio; }
         }
         #endregion
     }
