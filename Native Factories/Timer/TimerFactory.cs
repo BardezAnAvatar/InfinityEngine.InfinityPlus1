93712bd1ddacbd7df133ddd7c4323fd5b2ad8c8e
Erik "Bardez" Frantz <bardez@bardez.net>
2012-03-27 00:21:03 +0000

    Added in a native code factory project, so that generic projects need not reference platform-specific objects directly
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C277

---
+++ Native Factories/Timer/TimerFactory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video;
+using Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video.Timer;
+
+namespace Bardez.Projects.InfinityPlus1.NativeFactories.Timer
+{
+    /// <summary>Factory for ITimer</summary>
+    public static class TimerFactory
+    {
+        /// <summary>Constructs the default timer for the environment</summary>
+        /// <returns>ITimer Instance</returns>
+        public static ITimer BuildTimer()
+        {
+            return new Win32MultimediaTimer();
+        }
+
+        /// <summary>Constructs the default timer for the environment</summary>
+        /// <param name="frequency">Frequency period of the timer interval</param>
+        /// <returns>ITimer Instance</returns>
+        public static ITimer BuildTimer(UInt32 frequency)
+        {
+            return new Win32MultimediaTimer(frequency);
+        }
+    }
+}
