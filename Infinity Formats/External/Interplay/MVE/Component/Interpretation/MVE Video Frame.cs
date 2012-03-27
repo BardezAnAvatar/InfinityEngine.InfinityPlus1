75edbcb3c0caf824421021d7e0e3e05952fb87cf
Erik "Bardez" Frantz <bardez@bardez.net>
2012-03-27 00:33:00 +0000

    Refactored to have this structure house its cached MediaBase frame reference
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C291

--- Infinity Formats/External/Interplay/MVE/Component/Interpretation/MVE Video Frame.cs
+++ Infinity Formats/External/Interplay/MVE/Component/Interpretation/MVE Video Frame.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 
 using Bardez.Projects.InfinityPlus1.FileFormats.External.Interplay.MVE.Component.Opcodes;
 using Bardez.Projects.InfinityPlus1.FileFormats.External.Interplay.MVE.Enum;
+using Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video;
 
-namespace Bardez.Projects.InfinityPlus1.FileFormats.External.Interplay.MVE.Component.Management
+namespace Bardez.Projects.InfinityPlus1.FileFormats.External.Interplay.MVE.Component.Interpretation
 {
     /// <summary>Represents a container for the data needed to process a single video frame (i.e.: the MVE SetDecodingMap and VideoData opcodes)</summary>
     /// <remarks>Does not contain a back-reference to the previous frame</remarks>
-    public class MveVideoFrame
+    public class MveVideoFrame : MveInterpretableChunk
     {
         #region Fields
         /// <summary>Represents a frame's decoding approaches for each 8x8 block of the frame</summary>
@@ -16,17 +17,32 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.External.Interplay.MVE.Compo
 
         /// <summary>Represents the binary encoded data of the frame</summary>
         public VideoData Data { get; set; }
+
+        /// <summary>Buffer to hold a decoded frame from the buffer</summary>
+        public Frame FrameBuffer { get; set; }
+
+        public Int32 FrameNumber;
         #endregion
 
 
         #region Construction
-        /// <summary>Definition constructor</summary>
+        /// <summary>Partial definition constructor</summary>
         /// <param name="decodingMap">SetDecodingMap opcode</param>
         /// <param name="data">VideoData Opcode</param>
         public MveVideoFrame(SetDecodingMap decodingMap, VideoData data)
         {
             this.DecodingMap = decodingMap;
             this.Data = data;
+        }
+
+        /// <summary>Partial definition constructor</summary>
+        /// <param name="decodingMap">SetDecodingMap opcode</param>
+        /// <param name="data">VideoData Opcode</param>
+        public MveVideoFrame(SetDecodingMap decodingMap, VideoData data, Int32 number)
+        {
+            this.DecodingMap = decodingMap;
+            this.Data = data;
+            this.FrameNumber = number;
         }
         #endregion
     }
