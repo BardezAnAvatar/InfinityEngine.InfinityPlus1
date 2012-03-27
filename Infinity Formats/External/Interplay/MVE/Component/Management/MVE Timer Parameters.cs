f10b323b14c09dcd9a1e43697237aa1b47881e22
Erik "Bardez" Frantz <bardez@bardez.net>
2012-03-27 00:34:27 +0000

    Added in a Decimal property representation
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C294

--- Infinity Formats/External/Interplay/MVE/Component/Management/MVE Timer Parameters.cs
+++ Infinity Formats/External/Interplay/MVE/Component/Management/MVE Timer Parameters.cs
@@ -20,19 +20,43 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.External.Interplay.MVE.Compo
         /// <summary>Frequency of frame refresh, in milliseconds</summary>
         public Int32 MillisecondDelay
         {
-            get { return Convert.ToInt32(this.FrameInterval * 1000); }
+            get { return Convert.ToInt32(this.FrameIntervalDouble); }
         }
 
         /// <summary>Frames per second rate of the media file</summary>
-        public Double FrameRate
+        public Double FrameRateDouble
         {
             get { return Convert.ToDouble(this.Numerator) / Convert.ToDouble(this.Denominator); }
         }
 
         /// <summary>Milliseconds per Frame of the media file</summary>
-        public Double FrameInterval
+        public Double FrameIntervalDouble
         {
             get { return Convert.ToDouble(this.Denominator) / Convert.ToDouble(this.Numerator) * 1000.0; }
+        }
+
+        /// <summary>Frames per second rate of the media file</summary>
+        public Decimal FrameRateDecimal
+        {
+            get { return Convert.ToDecimal(this.Numerator) / Convert.ToDecimal(this.Denominator); }
+        }
+
+        /// <summary>Milliseconds per Frame of the media file</summary>
+        public Decimal FrameIntervalDecimal
+        {
+            get { return Convert.ToDecimal(this.Denominator) / Convert.ToDecimal(this.Numerator) * 1000M; }
+        }
+        #endregion
+
+
+        #region Construction
+        /// <summary>Definition constructor</summary>
+        /// <param name="numerator">Frame rate numerator</param>
+        /// <param name="denominator">Frame rate denominator</param>
+        public MveTimerParams(Int64 numerator, Int64 denominator)
+        {
+            this.Numerator = numerator;
+            this.Denominator = denominator;
         }
         #endregion
     }
