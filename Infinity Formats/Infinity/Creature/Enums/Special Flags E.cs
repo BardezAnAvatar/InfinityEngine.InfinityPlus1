77f2a3630cb4b4a116664aafbb2450a33314472e
Erik "Bardez" Frantz <bardez@bardez.net>
2012-04-17 05:39:06 +0000

    Added in new field discovered by Avenger
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C346

---
+++ Infinity Formats/Infinity/Creature/Enums/Special Flags E.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Creature.Enums
+{
+    /// <summary>Represents additional special IWD2 flags</summary>
+    /// <remarks>Discovered by Avenger</remarks>
+    [Flags]
+    public enum SpecialFlags3E : byte /* Byte */
+    {
+        /// <summary>Automatically succeed concentration rolls</summary>
+        [Description("Automatically succeed concentration rolls")]
+        ConcentrationSuccess = 1,
+
+        /// <summary>Critical hit damage is not applied</summary>
+        [Description("Critical hit damage is not applied")]
+        ImmunteToCriticalHits = 2,
+
+        /// <summary>Cannot take levels in Paladin, if you chose a confliciting class</summary>
+        [Description("Cannot take levels in Paladin, if you chose a confliciting class")]
+        CannotTakeLevelsInPaladin = 4,
+
+        /// <summary>Cannot take levels in Monk, if you chose a confliciting class</summary>
+        [Description("Cannot take levels in Monk, if you chose a confliciting class")]
+        CannotTakeLevelsInMonk = 8,
+    }
+}
