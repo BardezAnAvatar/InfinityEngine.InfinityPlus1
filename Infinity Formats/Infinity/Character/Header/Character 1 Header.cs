9bfc49970c4f910da3a3a24d3b7294420d897a77
Erik "Bardez" Frantz <bardez@bardez.net>
2012-04-17 05:33:15 +0000

    Moved quickslots into their own reusable data structure (reused in *.gam)
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C341

--- Infinity Formats/Infinity/Character/Header/Character 1 Header.cs
+++ Infinity Formats/Infinity/Character/Header/Character 1 Header.cs
@@ -12,95 +12,39 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Character.Header
     /// <summary>Character header version 1</summary>
     public class Character1Header : CharacterHeaderBase
     {
+        #region Constants
         /// <summary>Binary size of the struct on disk</summary>
         public Int32 StructSize
         {
-            get { return  0x64; }
+            get { return 0x64; }
         }
 
-        #region Members
-        /// <summary>Field indicating whether or not to show the associated quickslot, evaluated as a Boolean</summary>
-        protected UInt16 showQuickWeaponSlot1;
-        
-        /// <summary>Field indicating whether or not to show the associated quickslot, evaluated as a Boolean</summary>
-        protected UInt16 showQuickWeaponSlot2;
-        
-        /// <summary>Field indicating whether or not to show the associated quickslot, evaluated as a Boolean</summary>
-        protected UInt16 showQuickWeaponSlot3;
-        
-        /// <summary>Field indicating whether or not to show the associated quickslot, evaluated as a Boolean</summary>
-        protected UInt16 showQuickWeaponSlot4;
-        
-        /// <summary>Field indicating whether or not to show the associated quickslot, evaluated as a Boolean</summary>
-        protected UInt16 showQuickItemSlot1;
-        
-        /// <summary>Field indicating whether or not to show the associated quickslot, evaluated as a Boolean</summary>
-        protected UInt16 showQuickItemSlot2;
-        
-        /// <summary>Field indicating whether or not to show the associated quickslot, evaluated as a Boolean</summary>
-        protected UInt16 showQuickItemSlot3;
+        /// <summary>Length of binary data to read for minor fields</summary>
+        private const Int32 ReadLength = 40;
         #endregion
 
-        #region Properties
-        /// <summary>Field indicating whether or not to show the associated quickslot, evaluated as a Boolean</summary>
-        public UInt16 ShowQuickWeaponSlot1
-        {
-            get { return this.showQuickWeaponSlot1; }
-            set { this.showQuickWeaponSlot1 = value; }
-        }
-        
-        /// <summary>Field indicating whether or not to show the associated quickslot, evaluated as a Boolean</summary>
-        public UInt16 ShowQuickWeaponSlot2
-        {
-            get { return this.showQuickWeaponSlot2; }
-            set { this.showQuickWeaponSlot2 = value; }
-        }
-        
-        /// <summary>Field indicating whether or not to show the associated quickslot, evaluated as a Boolean</summary>
-        public UInt16 ShowQuickWeaponSlot3
-        {
-            get { return this.showQuickWeaponSlot3; }
-            set { this.showQuickWeaponSlot3 = value; }
-        }
-        
-        /// <summary>Field indicating whether or not to show the associated quickslot, evaluated as a Boolean</summary>
-        public UInt16 ShowQuickWeaponSlot4
-        {
-            get { return this.showQuickWeaponSlot4; }
-            set { this.showQuickWeaponSlot4 = value; }
-        }
 
-        /// <summary>Field indicating whether or not to show the associated quickslot, evaluated as a Boolean</summary>
-        public UInt16 ShowQuickItemSlot1
-        {
-            get { return this.showQuickItemSlot1; }
-            set { this.showQuickItemSlot1 = value; }
-        }
-        
-        /// <summary>Field indicating whether or not to show the associated quickslot, evaluated as a Boolean</summary>
-        public UInt16 ShowQuickItemSlot2
-        {
-            get { return this.showQuickItemSlot2; }
-            set { this.showQuickItemSlot2 = value; }
-        }
-        
-        /// <summary>Field indicating whether or not to show the associated quickslot, evaluated as a Boolean</summary>
-        public UInt16 ShowQuickItemSlot3
-        {
-            get { return this.showQuickItemSlot3; }
-            set { this.showQuickItemSlot3 = value; }
-        }
+        #region Fields
+        /// <summary>Represents the collection of quickslots for this character</summary>
+        public CharacterSlots_v1 QuickSlots { get; set; }
         #endregion
-        
+
+
         #region Constructor(s)
         /// <summary>Default constructor</summary>
         public Character1Header()
         {
-            this.quickSpell1 = null;
-            this.quickSpell2 = null;
-            this.quickSpell3 = null;
+            this.QuickSlots = null;
+        }
+
+        /// <summary>Instantiates reference types</summary>
+        public override void Initialize()
+        {
+            base.Initialize();
+            this.QuickSlots = new CharacterSlots_v1();
         }
         #endregion
+
 
         #region IO method implemetations
         /// <summary>This public method reads file format data structure from the output stream, after the signature has already been read.</summary>
@@ -111,28 +55,12 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Character.Header
             this.Initialize();
 
             //read remiander
-            Byte[] header = ReusableIO.BinaryRead(input, this.StructSize - 8);
+            Byte[] header = ReusableIO.BinaryRead(input, Character1Header.ReadLength);
 
             this.Name.Value = ReusableIO.ReadStringFromByteArray(header, 0, CultureConstants.CultureCodeEnglish, 32);
-            this.offsetCreature = ReusableIO.ReadUInt32FromArray(header, 32);
-            this.lengthCreature = ReusableIO.ReadUInt32FromArray(header, 36);
-            this.quickWeaponSlotIndex1 = ReusableIO.ReadUInt16FromArray(header, 40);
-            this.quickWeaponSlotIndex2 = ReusableIO.ReadUInt16FromArray(header, 42);
-            this.quickWeaponSlotIndex3 = ReusableIO.ReadUInt16FromArray(header, 44);
-            this.quickWeaponSlotIndex4 = ReusableIO.ReadUInt16FromArray(header, 46);
-            this.showQuickWeaponSlot1 = ReusableIO.ReadUInt16FromArray(header, 48);
-            this.showQuickWeaponSlot2 = ReusableIO.ReadUInt16FromArray(header, 50);
-            this.showQuickWeaponSlot3 = ReusableIO.ReadUInt16FromArray(header, 52);
-            this.showQuickWeaponSlot4 = ReusableIO.ReadUInt16FromArray(header, 54);
-            this.quickSpell1.ResRef = ReusableIO.ReadStringFromByteArray(header, 56, CultureConstants.CultureCodeEnglish);
-            this.quickSpell2.ResRef = ReusableIO.ReadStringFromByteArray(header, 64, CultureConstants.CultureCodeEnglish);
-            this.quickSpell3.ResRef = ReusableIO.ReadStringFromByteArray(header, 72, CultureConstants.CultureCodeEnglish);
-            this.quickItemSlotIndex1 = ReusableIO.ReadInt16FromArray(header, 80);
-            this.quickItemSlotIndex2 = ReusableIO.ReadInt16FromArray(header, 82);
-            this.quickItemSlotIndex3 = ReusableIO.ReadInt16FromArray(header, 84);
-            this.showQuickItemSlot1 = ReusableIO.ReadUInt16FromArray(header, 86);
-            this.showQuickItemSlot2 = ReusableIO.ReadUInt16FromArray(header, 88);
-            this.showQuickItemSlot3 = ReusableIO.ReadUInt16FromArray(header, 90);
+            this.OffsetCreature = ReusableIO.ReadUInt32FromArray(header, 32);
+            this.LengthCreature = ReusableIO.ReadUInt32FromArray(header, 36);
+            this.QuickSlots.ReadBody(input);
         }
 
         /// <summary>This public method writes the file format to the output stream.</summary>
@@ -142,27 +70,12 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Character.Header
             ReusableIO.WriteStringToStream(this.signature, output, CultureConstants.CultureCodeEnglish, false, 4);
             ReusableIO.WriteStringToStream(this.version, output, CultureConstants.CultureCodeEnglish, false, 4);
             ReusableIO.WriteStringToStream(this.Name.Value, output, CultureConstants.CultureCodeEnglish, false, 32);
-            ReusableIO.WriteUInt32ToStream(this.offsetCreature, output);
-            ReusableIO.WriteUInt32ToStream(this.lengthCreature, output);
-            ReusableIO.WriteUInt16ToStream(this.quickWeaponSlotIndex1, output);
-            ReusableIO.WriteUInt16ToStream(this.quickWeaponSlotIndex2, output);
-            ReusableIO.WriteUInt16ToStream(this.quickWeaponSlotIndex3, output);
-            ReusableIO.WriteUInt16ToStream(this.quickWeaponSlotIndex4, output);
-            ReusableIO.WriteUInt16ToStream(this.showQuickWeaponSlot1, output);
-            ReusableIO.WriteUInt16ToStream(this.showQuickWeaponSlot2, output);
-            ReusableIO.WriteUInt16ToStream(this.showQuickWeaponSlot3, output);
-            ReusableIO.WriteUInt16ToStream(this.showQuickWeaponSlot4, output);
-            ReusableIO.WriteStringToStream(this.quickSpell1.ResRef, output, CultureConstants.CultureCodeEnglish);
-            ReusableIO.WriteStringToStream(this.quickSpell2.ResRef, output, CultureConstants.CultureCodeEnglish);
-            ReusableIO.WriteStringToStream(this.quickSpell3.ResRef, output, CultureConstants.CultureCodeEnglish);
-            ReusableIO.WriteInt16ToStream(this.quickItemSlotIndex1, output);
-            ReusableIO.WriteInt16ToStream(this.quickItemSlotIndex2, output);
-            ReusableIO.WriteInt16ToStream(this.quickItemSlotIndex3, output);
-            ReusableIO.WriteUInt16ToStream(this.showQuickItemSlot1, output);
-            ReusableIO.WriteUInt16ToStream(this.showQuickItemSlot2, output);
-            ReusableIO.WriteUInt16ToStream(this.showQuickItemSlot3, output);
+            ReusableIO.WriteUInt32ToStream(this.OffsetCreature, output);
+            ReusableIO.WriteUInt32ToStream(this.LengthCreature, output);
+            this.QuickSlots.Write(output);
         }
         #endregion
+
 
         #region ToString() helpers
         /// <summary>This method overrides the default ToString() method, printing the member data line by line</summary>
@@ -184,49 +97,11 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Character.Header
             builder.Append(StringFormat.ToStringAlignment("Name"));
             builder.Append(this.Name.Value);
             builder.Append(StringFormat.ToStringAlignment("Creature offset"));
-            builder.Append(this.offsetCreature);
+            builder.Append(this.OffsetCreature);
             builder.Append(StringFormat.ToStringAlignment("Creature length"));
-            builder.Append(this.lengthCreature);
-            builder.Append(StringFormat.ToStringAlignment("Quick Weapon Index 1"));
-            builder.Append(this.quickWeaponSlotIndex1);
-            builder.Append(StringFormat.ToStringAlignment("Quick Weapon Index 2"));
-            builder.Append(this.quickWeaponSlotIndex2);
-            builder.Append(StringFormat.ToStringAlignment("Quick Weapon Index 3"));
-            builder.Append(this.quickWeaponSlotIndex3);
-            builder.Append(StringFormat.ToStringAlignment("Quick Weapon Index 4"));
-            builder.Append(this.quickWeaponSlotIndex4);
-            builder.Append(StringFormat.ToStringAlignment("Show quick weapon slot 1"));
-            builder.Append(this.showQuickWeaponSlot1);
-            builder.Append(StringFormat.ToStringAlignment("Show quick weapon slot 2"));
-            builder.Append(this.showQuickWeaponSlot2);
-            builder.Append(StringFormat.ToStringAlignment("Show quick weapon slot 3"));
-            builder.Append(this.showQuickWeaponSlot3);
-            builder.Append(StringFormat.ToStringAlignment("Show quick weapon slot 4"));
-            builder.Append(this.showQuickWeaponSlot4);
-            builder.Append(StringFormat.ToStringAlignment("Quick spell ResRef 1"));
-            builder.Append("'");
-            builder.Append(this.quickSpell1.ZResRef);
-            builder.Append("'");
-            builder.Append(StringFormat.ToStringAlignment("Quick spell ResRef 2"));
-            builder.Append("'");
-            builder.Append(this.quickSpell2.ZResRef);
-            builder.Append("'");
-            builder.Append(StringFormat.ToStringAlignment("Quick spell ResRef 3"));
-            builder.Append("'");
-            builder.Append(this.quickSpell3.ZResRef);
-            builder.Append("'");
-            builder.Append(StringFormat.ToStringAlignment("Quick item index 1"));
-            builder.Append(this.quickItemSlotIndex1);
-            builder.Append(StringFormat.ToStringAlignment("Quick item index 2"));
-            builder.Append(this.quickItemSlotIndex2);
-            builder.Append(StringFormat.ToStringAlignment("Quick item index 3"));
-            builder.Append(this.quickItemSlotIndex3);
-            builder.Append(StringFormat.ToStringAlignment("Show quick item 1"));
-            builder.Append(this.showQuickItemSlot1);
-            builder.Append(StringFormat.ToStringAlignment("Show quick item 2"));
-            builder.Append(this.showQuickItemSlot2);
-            builder.Append(StringFormat.ToStringAlignment("Show quick item 3"));
-            builder.Append(this.showQuickItemSlot3);
+            builder.Append(this.LengthCreature);
+
+            builder.Append(this.QuickSlots.ToString());
 
             return builder.ToString();
         }
--- Infinity Formats/Infinity/Character/Header/Character Header Base.cs
+++ Infinity Formats/Infinity/Character/Header/Character Header Base.cs
@@ -13,157 +13,26 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Character.Header
     /// <summary>Character header base class</summary>
     public abstract class CharacterHeaderBase : InfinityFormat
     {
-        #region Members
-        /// <summary>Offset to the creature structure</summary>
-        protected UInt32 offsetCreature;
-
-        /// <summary>Length of the creature structure</summary>
-        protected UInt32 lengthCreature;
-        
-        /// <summary>Index into SLOTS.IDS for Quick Weapon 1</summary>
-        /// <value>(0xFFFF = none)</value>
-        protected UInt16 quickWeaponSlotIndex1;
-        
-        /// <summary>Index into SLOTS.IDS for Quick Weapon 2</summary>
-        /// <value>(0xFFFF = none)</value>
-        protected UInt16 quickWeaponSlotIndex2;
-        
-        /// <summary>Index into SLOTS.IDS for Quick Weapon 3</summary>
-        /// <value>(0xFFFF = none)</value>
-        protected UInt16 quickWeaponSlotIndex3;
-        
-        /// <summary>Index into SLOTS.IDS for Quick Weapon 4</summary>
-        /// <value>(0xFFFF = none)</value>
-        protected UInt16 quickWeaponSlotIndex4;
-
-        /// <summary>Resource reference to first quick spell</summary>
-        protected ResourceReference quickSpell1;
-        
-        /// <summary>Resource reference to second quick spell</summary>
-        protected ResourceReference quickSpell2;
-        
-        /// <summary>Resource reference to third quick spell</summary>
-        protected ResourceReference quickSpell3;
-        
-        /// <summary>Index into SLOTS.IDS for Quick Item 1</summary>
-        /// <value>(0xFFFF = none)</value>
-        protected Int16 quickItemSlotIndex1;
-        
-        /// <summary>Index into SLOTS.IDS for Quick Item 2</summary>
-        /// <value>(0xFFFF = none)</value>
-        protected Int16 quickItemSlotIndex2;
-        
-        /// <summary>Index into SLOTS.IDS for Quick Item 3</summary>
-        /// <value>(0xFFFF = none)</value>
-        protected Int16 quickItemSlotIndex3;
-        #endregion
-
-        #region Properties
+        #region Fields
         /// <summary>Character name, up to 32 bytes</summary>
         public ZString Name { get; set; }
 
         /// <summary>Offset to the creature structure</summary>
-        public UInt32 OffsetCreature
-        {
-            get { return this.offsetCreature; }
-            set { this.offsetCreature = value; }
-        }
+        public UInt32 OffsetCreature { get; set; }
 
         /// <summary>Length of the creature structure</summary>
-        public UInt32 LengthCreature
-        {
-            get { return this.lengthCreature; }
-            set { this.lengthCreature = value; }
-        }
-        
-        /// <summary>Index into SLOTS.IDS for Quick Weapon 1</summary>
-        /// <value>(0xFFFF = none)</value>
-        public UInt16 QuickWeaponSlotIndex1
-        {
-            get { return this.quickWeaponSlotIndex1; }
-            set { this.quickWeaponSlotIndex1 = value; }
-        }
-        
-        /// <summary>Index into SLOTS.IDS for Quick Weapon 2</summary>
-        /// <value>(0xFFFF = none)</value>
-        public UInt16 QuickWeaponSlotIndex2
-        {
-            get { return this.quickWeaponSlotIndex2; }
-            set { this.quickWeaponSlotIndex2 = value; }
-        }
-        
-        /// <summary>Index into SLOTS.IDS for Quick Weapon 3</summary>
-        /// <value>(0xFFFF = none)</value>
-        public UInt16 QuickWeaponSlotIndex3
-        {
-            get { return this.quickWeaponSlotIndex3; }
-            set { this.quickWeaponSlotIndex3 = value; }
-        }
-        
-        /// <summary>Index into SLOTS.IDS for Quick Weapon 4</summary>
-        /// <value>(0xFFFF = none)</value>
-        public UInt16 QuickWeaponSlotIndex4
-        {
-            get { return this.quickWeaponSlotIndex4; }
-            set { this.quickWeaponSlotIndex4 = value; }
-        }
-        
-        /// <summary>Resource reference to first quick spell</summary>
-        public ResourceReference QuickSpell1
-        {
-            get { return this.quickSpell1; }
-            set { this.quickSpell1 = value; }
-        }
-        
-        /// <summary>Resource reference to second quick spell</summary>
-        public ResourceReference QuickSpell2
-        {
-            get { return this.quickSpell2; }
-            set { this.quickSpell2 = value; }
-        }
-        
-        /// <summary>Resource reference to third quick spell</summary>
-        public ResourceReference QuickSpell3
-        {
-            get { return this.quickSpell3; }
-            set { this.quickSpell3 = value; }
-        }
-        
-        /// <summary>Index into SLOTS.IDS for Quick Item 1</summary>
-        /// <value>(0xFFFF = none)</value>
-        public Int16 QuickItemSlotIndex1
-        {
-            get { return this.quickItemSlotIndex1; }
-            set { this.quickItemSlotIndex1 = value; }
-        }
-        
-        /// <summary>Index into SLOTS.IDS for Quick Item 2</summary>
-        /// <value>(0xFFFF = none)</value>
-        public Int16 QuickItemSlotIndex2
-        {
-            get { return this.quickItemSlotIndex2; }
-            set { this.quickItemSlotIndex2 = value; }
-        }
-        
-        /// <summary>Index into SLOTS.IDS for Quick Item 3</summary>
-        /// <value>(0xFFFF = none)</value>
-        public Int16 QuickItemSlotIndex3
-        {
-            get { return this.quickItemSlotIndex3; }
-            set { this.quickItemSlotIndex3 = value; }
-        }
+        public UInt32 LengthCreature { get; set; }
         #endregion
+
         
-        #region Constructor(s)
+        #region Construction
         /// <summary>Instantiates reference types</summary>
         public override void Initialize()
         {
-            this.quickSpell1 = new ResourceReference();
-            this.quickSpell2 = new ResourceReference();
-            this.quickSpell3 = new ResourceReference();
             this.Name = new ZString();
         }
         #endregion
+
 
         #region ToString() helpers
         /// <summary>This method overrides the default ToString() method, printing the member data line by line</summary>
