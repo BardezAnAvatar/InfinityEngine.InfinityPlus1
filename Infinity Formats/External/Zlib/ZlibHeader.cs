f2448af60798a27009ecade05729f04940aed21e
Erik "Bardez" Frantz <bardez@bardez.net>
2012-04-17 03:06:04 +0000

    Added in ZLib-compressed header for ZLib formatted files
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C334

---
+++ Infinity Formats/External/Zlib/ZlibHeader.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using Bardez.Projects.InfinityPlus1.FileFormats.Basic;
+using Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Base;
+using Bardez.Projects.ReusableCode;
+
+namespace Bardez.Projects.InfinityPlus1.FileFormats.External.Zlib
+{
+    /// <summary>Represents the header of a ZLIB compressed file file</summary>
+    public class ZlibHeader : InfinityFormat
+    {
+        #region Constants
+        /// <summary>Binary size of the struct on disk</summary>
+        private const Int32 StructSize = 12;
+        #endregion
+
+
+        #region Fields
+        /// <summary>Uncompressed size of the zlib stream</summary>
+        public UInt32 Size { get; set; }
+        #endregion
+
+
+        #region Construction
+        /// <summary>Instantiates reference types</summary>
+        public override void Initialize() { }
+        #endregion
+
+
+        #region IO method implemetations
+        /// <summary>This public method reads file format data structure from the input stream, after the signature has already been read.</summary>
+        /// <param name="input">Stream to read from</param>
+        public override void ReadBody(Stream input)
+        {
+            this.Initialize();
+
+            Byte[] header = ReusableIO.BinaryRead(input, ZlibHeader.StructSize - 8);
+            this.Size = ReusableIO.ReadUInt32FromArray(header, 0);
+        }
+
+        /// <summary>This public method writes the file format to the output stream.</summary>
+        /// <param name="output">Stream to write to</param>
+        public override void Write(Stream output)
+        {
+            ReusableIO.WriteStringToStream(this.signature, output, CultureConstants.CultureCodeEnglish, false, 4);
+            ReusableIO.WriteStringToStream(this.version, output, CultureConstants.CultureCodeEnglish, false, 4);
+            ReusableIO.WriteUInt32ToStream(this.Size, output);
+        }
+        #endregion
+
+
+        #region ToString() methods
+        /// <summary>This method overrides the default ToString() method, printing the member data line by line</summary>
+        /// <returns>A string containing the values and descriptions of all values in this class</returns>
+        public override String ToString()
+        {
+            return this.ToString(true);
+        }
+
+        /// <summary>This method complements the default ToString() method, printing the member data line by line</summary>
+        /// <param name="showType">Boolean indicating whether or not to display the leading description line.</param>
+        /// <returns>A string containing the values and descriptions of all values in this class</returns>
+        public String ToString(Boolean showType)
+        {
+            String header = this.GetVersionString();
+
+            if (showType)
+                header += this.GetStringRepresentation();
+            else
+                header = this.GetStringRepresentation();
+
+            return header;
+        }
+
+        /// <summary>Returns the printable read-friendly version of the store format</summary>
+        protected String GetVersionString()
+        {
+            return "ZLIB-compressed file header:";
+        }
+
+        /// <summary>This method performs the bulk of work for a ToString() implementation that would output to console or similar.</summary>
+        /// <returns>A string, formatted largely for console, that describes the item's contents.</returns>
+        protected String GetStringRepresentation()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(StringFormat.ToStringAlignment("Signature"));
+            builder.Append(this.signature);
+            builder.Append(StringFormat.ToStringAlignment("Version"));
+            builder.Append(this.version);
+            builder.Append(StringFormat.ToStringAlignment("Size"));
+            builder.Append(this.Size);
+
+            return builder.ToString();
+        }
+        #endregion
+    }
+}
