1f452aa6be40bd96e5fad100113465195f143b06
Erik "Bardez" Frantz <bardez@bardez.net>
2012-04-17 05:48:16 +0000

    Added in zlib-compressed BAM render control
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C355

---
+++ Test/WinForm/BAM/BAMC Render Test Control.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+using Bardez.Projects.Configuration;
+using Bardez.Projects.InfinityPlus1.FileFormats.Infinity.BioWareAnimation;
+using Bardez.Projects.InfinityPlus1.Test.WinForm;
+
+namespace Bardez.Projects.InfinityPlus1.Test.WinForm.BAM
+{
+    /// <summary>Rendering test animation control for BAMC files</summary>
+    public class BamcRenderTestControl : HarnessAnimationCollectionTestControlBase<BioWareAnimationCompressed_v1>
+    {
+        #region Constants
+        /// <summary>Constant key to look up in app.config</summary>
+        protected const String configKey = "Test.BAMC.Path";
+        #endregion
+
+
+        #region Construction
+        /// <summary>Default constructor</summary>
+        public BamcRenderTestControl() : base() { }
+        #endregion
+
+
+        #region Helper methods
+        /// <summary>Gets the paths to test from the config file</summary>
+        /// <returns>An IList of Strings for file paths</returns>
+        protected override IList<String> GetPaths()
+        {
+            return ConfigurationHandlerMulti.GetSettingValues(BamcRenderTestControl.configKey);
+        }
+
+        /// <summary>Overridable method to set detail of a frame from within DecodeSingleImageCollection</summary>
+        /// <param name="frame">ImageReference to set details for</param>
+        /// <param name="container">ImageCollection format that contains animations and frames</param>
+        /// <param name="frameIndex">Frame number setting the details for</param>
+        protected override void SetFrameDetails(ImageReference frame, BioWareAnimationCompressed_v1 container, Int32 frameIndex)
+        {
+            frame.RenderOriginX = container.File.MaxCenterX + frame.RenderOriginX;
+            frame.RenderOriginY = container.File.MaxCenterY + frame.RenderOriginY;
+        }
+        #endregion
+    }
+}
