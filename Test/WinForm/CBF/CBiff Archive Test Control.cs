adb0fb37ce2d47406c9e6e6dd35ea355ec5d69c2
Erik "Bardez" Frantz <bardez@bardez.net>
2012-04-18 05:39:04 +0000

    Added in CBiff test control
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C369

---
+++ Test/WinForm/CBF/CBiff Archive Test Control.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+
+using Bardez.Projects.InfinityPlus1.Test.Harnesses.BioWareIndexFileFormat.CBiff;
+using Bardez.Projects.InfinityPlus1.Utility;
+
+namespace Bardez.Projects.InfinityPlus1.Test.WinForm.CBF
+{
+    /// <summary>User control for testing the IWD C-BIFF archive</summary>
+    //TODO: Create an extractable interface for both SAV and BIFF archives, to expose to a control for
+    //  resource extraction
+    public class CBiffArchiveTestControl : HarnessFileBaseTestControlBase<CBiffArchiveTest>
+    {
+        /// <summary>Default constructor</summary>
+        public CBiffArchiveTestControl()
+        {
+            this.InitializeComponent();
+            this.Harness = new CBiffArchiveTest();
+            this.InitializeControlFields();
+        }
+    }
+}
