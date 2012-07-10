86280d54fa2448123fe58e1210cbdb843e0d7aac
Erik "Bardez" Frantz <bardez@bardez.net>
2012-07-10 02:56:34 +0000

    Converted the Resource managers to static instances with disposers that are not reliant on an instance
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C410

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -10,7 +10,7 @@ namespace Bardez.Projects.InfinityPlus1.Tester
     {
         /// <summary>Driving main method application entry point</summary>
         /// <param name="args">String Array of application arguments</param>
-        [STAThread]
+        [MTAThread]
         internal static void Main(String[] args)
         {
             ExceptionManager.AttachManagerForWinForms();
@@ -19,10 +19,12 @@ namespace Bardez.Projects.InfinityPlus1.Tester
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new TestUI());
             //Application.Run(new Scratch());
+            //Application.Run(new ScratchSingleD2D());
+
 
             //I need a singleton resource container for end form disposal; set up when started, released when closing. for now, just dispose here.
-            Bardez.Projects.InfinityPlus1.Output.Audio.XAudio2Output.Instance.Dispose();
-            Bardez.Projects.InfinityPlus1.Output.Visual.Direct2dResourceManager.Instance.Dispose(); //clear out all the device-dependent resources
+            Bardez.Projects.InfinityPlus1.Output.Audio.XAudio2Output.DisposeInstance();
+            Bardez.Projects.InfinityPlus1.Output.Visual.Direct2dResourceManager.DisposeInstance(); //clear out all the device-dependent resources
         }
     }
 }
