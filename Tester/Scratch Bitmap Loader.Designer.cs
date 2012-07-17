75d4405e96e53efb3068fce3a3234bfba659ba05
Erik "Bardez" Frantz <bardez@bardez.net>
2012-07-17 01:08:56 +0000

    Added a new form for debugging a memory leak, now obsolete
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C423

---
+++ Tester/Scratch Bitmap Loader.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Bardez.Projects.InfinityPlus1.Tester
+{
+    partial class ScratchBitmapLoader
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoadBitmapScale = new System.Windows.Forms.Button();
+            this.listboxLoading = new System.Windows.Forms.ListBox();
+            this.btnLoadBitmapIntoLibAVPicture = new System.Windows.Forms.Button();
+            this.btnDeclareAndDisposeLibAVPicture = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnLoadBitmapScale
+            // 
+            this.btnLoadBitmapScale.Location = new System.Drawing.Point(13, 13);
+            this.btnLoadBitmapScale.Name = "btnLoadBitmapScale";
+            this.btnLoadBitmapScale.Size = new System.Drawing.Size(114, 23);
+            this.btnLoadBitmapScale.TabIndex = 0;
+            this.btnLoadBitmapScale.Text = "Load Bitmap && Scale";
+            this.btnLoadBitmapScale.UseVisualStyleBackColor = true;
+            this.btnLoadBitmapScale.Click += new System.EventHandler(this.btnLoadBitmapScale_Click);
+            // 
+            // listboxLoading
+            // 
+            this.listboxLoading.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listboxLoading.FormattingEnabled = true;
+            this.listboxLoading.Location = new System.Drawing.Point(12, 43);
+            this.listboxLoading.Name = "listboxLoading";
+            this.listboxLoading.Size = new System.Drawing.Size(465, 264);
+            this.listboxLoading.TabIndex = 1;
+            // 
+            // btnLoadBitmapIntoLibAVPicture
+            // 
+            this.btnLoadBitmapIntoLibAVPicture.Location = new System.Drawing.Point(133, 14);
+            this.btnLoadBitmapIntoLibAVPicture.Name = "btnLoadBitmapIntoLibAVPicture";
+            this.btnLoadBitmapIntoLibAVPicture.Size = new System.Drawing.Size(153, 23);
+            this.btnLoadBitmapIntoLibAVPicture.TabIndex = 2;
+            this.btnLoadBitmapIntoLibAVPicture.Text = "Load BMP into LibAVPicture";
+            this.btnLoadBitmapIntoLibAVPicture.UseVisualStyleBackColor = true;
+            this.btnLoadBitmapIntoLibAVPicture.Click += new System.EventHandler(this.btnLoadBitmapIntoLibAVPicture_Click);
+            // 
+            // btnDeclareAndDisposeLibAVPicture
+            // 
+            this.btnDeclareAndDisposeLibAVPicture.Location = new System.Drawing.Point(292, 14);
+            this.btnDeclareAndDisposeLibAVPicture.Name = "btnDeclareAndDisposeLibAVPicture";
+            this.btnDeclareAndDisposeLibAVPicture.Size = new System.Drawing.Size(182, 23);
+            this.btnDeclareAndDisposeLibAVPicture.TabIndex = 3;
+            this.btnDeclareAndDisposeLibAVPicture.Text = "Instantiate && Dispose LibAVPicture";
+            this.btnDeclareAndDisposeLibAVPicture.UseVisualStyleBackColor = true;
+            this.btnDeclareAndDisposeLibAVPicture.Click += new System.EventHandler(this.btnDeclareAndDisposeLibAVPicture_Click);
+            // 
+            // ScratchBitmapLoader
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 317);
+            this.Controls.Add(this.btnDeclareAndDisposeLibAVPicture);
+            this.Controls.Add(this.btnLoadBitmapIntoLibAVPicture);
+            this.Controls.Add(this.listboxLoading);
+            this.Controls.Add(this.btnLoadBitmapScale);
+            this.Name = "ScratchBitmapLoader";
+            this.Text = "Scrach Memory Leak explorer in SW Scale";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoadBitmapScale;
+        private System.Windows.Forms.ListBox listboxLoading;
+        private System.Windows.Forms.Button btnLoadBitmapIntoLibAVPicture;
+        private System.Windows.Forms.Button btnDeclareAndDisposeLibAVPicture;
+
+    }
+}
---
+++ Tester/Scratch Bitmap Loader.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+using Bardez.Projects.Configuration;
+using Bardez.Projects.InfinityPlus1.FileFormats.External.Image;
+using Bardez.Projects.InfinityPlus1.FileFormats.External.Image.Bitmap;
+using Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video.Enums;
+using Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video.Pixels.Enums;
+using Bardez.Projects.MultiMedia.LibAV;
+using Bardez.Projects.ReusableCode;
+
+namespace Bardez.Projects.InfinityPlus1.Tester
+{
+    public partial class ScratchBitmapLoader : Form
+    {
+        #region Constants
+        /// <summary>Constant key to look up in app.config</summary>
+        protected const String configKey = "Test.BMP.MemLeak.Path";
+        #endregion
+
+
+        #region Fields
+        public Int32 bitmapCount, memoryBitmapCount, pictureCount;
+        #endregion
+
+
+        #region Events
+        public ScratchBitmapLoader()
+        {
+            this.bitmapCount = 0;
+            this.memoryBitmapCount = 0;
+            this.pictureCount = 0;
+
+            InitializeComponent();
+        }
+
+        protected void btnLoadBitmapScale_Click(Object sender, EventArgs e)
+        {
+            this.LoadBitmap();
+        }
+
+        protected void btnLoadBitmapIntoLibAVPicture_Click(object sender, EventArgs e)
+        {
+            this.CopyLibAVPictureFromMemory();
+        }
+
+        protected void btnDeclareAndDisposeLibAVPicture_Click(object sender, EventArgs e)
+        {
+            this.InstantiateLibAVPicture();
+        }
+        #endregion
+
+
+        #region Methods
+        protected void LoadBitmap()
+        {
+            String path = ConfigurationHandlerMulti.GetSettingValue(ScratchBitmapLoader.configKey);
+
+            DeviceIndependentBitmap bitmap = new DeviceIndependentBitmap();
+            using (FileStream fs = ReusableIO.OpenFile(path))
+                bitmap.Read(fs);
+
+            using (MemoryStream ms = bitmap.PixelData.GetPixelData(PixelFormat.RGBA_B8G8R8A8, ScanLineOrder.TopDown, 0, 0))
+            {
+                String loggedMessage = String.Format("Loading DIBimap # {0}", ++this.bitmapCount);
+                this.listboxLoading.Items.Add(loggedMessage);
+            }
+        }
+
+        protected void InstantiateLibAVPicture()
+        {
+            using (LibAVPicture test = LibAVPicture.BuildPicture(LibAVPictureDetail.Build(1080, 1920, LibAVPixelFormat.PIX_FMT_BGRA)))
+            {
+                String loggedMessage = String.Format("Intantiating LibAVPicture # {0}", ++this.pictureCount);
+                this.listboxLoading.Items.Add(loggedMessage);
+            }
+        }
+
+        protected void CopyLibAVPictureFromMemory()
+        {
+            String path = ConfigurationHandlerMulti.GetSettingValue(ScratchBitmapLoader.configKey);
+
+            DeviceIndependentBitmap bitmap = new DeviceIndependentBitmap();
+            using (FileStream fs = ReusableIO.OpenFile(path))
+                bitmap.Read(fs);
+
+            MemoryStream ms = bitmap.PixelData.NativeBinaryData;
+            LibAVPictureDetail detail = LibAVPictureDetail.Build(bitmap.BitmapInfo.Width, bitmap.BitmapInfo.Height, LibAVPixelFormat.PIX_FMT_BGR24);
+
+            using (LibAVPicture test = LibAVPicture.BuildPicture(detail, ms))
+            {
+                String loggedMessage = String.Format("Intantiating LibAVPicture from DIBitmap # {0}", ++this.memoryBitmapCount);
+                this.listboxLoading.Items.Add(loggedMessage);
+            }
+        }
+        #endregion
+    }
+}
