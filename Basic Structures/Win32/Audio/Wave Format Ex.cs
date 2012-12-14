6af25883b3127135d5d1ab7bf30915f21725fde2
Erik "Bardez" Frantz <bardez@bardez.net>
2012-12-14 06:14:29 +0000

    I really liked the C# implementation of the Win32 structures. The existing C++/CLI implementation had no reason to be in C++/CLI since its unmanaged code was internal, and those internal references had to be broken out into other projects. So, I re-implemented in C#. Later, I renamed the project so that I could include some more basic things (like Vectors) into a basic struct.
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C443

---
+++ Basic Structures/Win32/Audio/Wave Format Ex.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Text;
+
+namespace Bardez.Projects.BasicStructures.Win32.Audio
+{
+    /// <summary>Managed representation of Win32 WAVEFORMATEX structure</summary>
+    public class WaveFormatEx
+    {
+	    #region Members
+		/// <summary>Integer identifier of the format</summary>
+		protected UInt16 formatTag;
+
+		/// <summary>Number of audio channels</summary>
+		protected UInt16 numberChannels;
+
+		/// <summary>Audio sample rate</summary>
+		protected UInt32 samplesPerSec;
+
+		/// <summary>Bytes per second (possibly approximate)</summary>
+		protected UInt32 averageBytesPerSec;
+
+		/// <summary>Size in bytes of a sample block (all channels)</summary>
+		protected UInt16 blockAlignment;
+
+		/// <summary>Size in bits of a single per-channel sample</summary>
+		/// <remarks>16 bit is optimal for XAudio2, 32 bit following, then other formats.</remarks>
+		protected UInt16 bitsPerSample;
+
+		/// <summary>
+		///		Bytes of extra data appended to this struct. Explains the length of bytes in addition to this struct,
+		///		essentially the extra size of child instances.
+		/// </summary>
+		protected UInt16 size;
+	    #endregion
+
+
+	    #region Properties
+		/// <summary>Integer identifier of the format</summary>
+		public UInt16 FormatTag
+		{
+            get { return this.formatTag; }
+            set { this.formatTag = value; }
+		}
+
+		/// <summary>Number of audio channels</summary>
+		public UInt16 NumberChannels
+		{
+            get { return this.numberChannels; }
+            set { this.numberChannels = value; }
+		}
+
+		/// <summary>Audio sample rate</summary>
+		public UInt32 SamplesPerSec
+		{
+            get { return this.samplesPerSec; }
+            set { this.samplesPerSec = value; }
+		}
+
+		/// <summary>Bytes per second (possibly approximate)</summary>
+		public UInt32 AverageBytesPerSec
+		{
+            get { return this.averageBytesPerSec; }
+            set { this.averageBytesPerSec = value; }
+		}
+
+		/// <summary>Size in bytes of a sample block (all channels)</summary>
+		public UInt16 BlockAlignment
+		{
+            get { return this.blockAlignment; }
+            set { this.blockAlignment = value; }
+		}
+
+		/// <summary>Size in bits of a single per-channel sample</summary>
+		/// <remarks>16 bit is optimal for XAudio2, 32 bit following, then other formats.</remarks>
+		public UInt16 BitsPerSample
+		{
+            get { return this.bitsPerSample; }
+            set { this.bitsPerSample = value; }
+		}
+
+		/// <summary>Bytes of extra data appended to this struct</summary>
+		public UInt16 Size
+        {
+            get { return this.size; }
+            set { this.size = value; }
+		}
+	    #endregion
+
+
+	    #region Construction
+	    /// <summary>Default constructor</summary>
+	    public WaveFormatEx() { }
+	    #endregion
+
+
+	    #region Methods
+	    /// <summary>Generates a descriptive string (to be displayed to the end user)</summary>
+	    /// <returns>A String representing the WaveFormatEx Object contents</returns>
+        public virtual String ToDescriptionString()
+        {
+	        String desc = null;
+
+	        StringBuilder builder = new StringBuilder();
+	        builder.Append("\tFormat tag:                        ");
+	        builder.Append(this.formatTag);
+	        builder.AppendLine(String.Empty);
+	        builder.Append("\tNumber of Channels:                ");
+	        builder.Append(this.numberChannels);
+	        builder.AppendLine(String.Empty);
+	        builder.Append("\tSamples per Second:                ");
+	        builder.Append(this.samplesPerSec);
+	        builder.AppendLine(String.Empty);
+	        builder.Append("\tAverage # of Bytes per Second:     ");
+	        builder.Append(this.averageBytesPerSec);
+	        builder.AppendLine(String.Empty);
+	        builder.Append("\tBlock Alignment:                   ");
+	        builder.Append(this.blockAlignment);
+	        builder.AppendLine(String.Empty);
+	        builder.Append("\tBits per Sample:                   ");
+	        builder.Append(this.bitsPerSample);
+	        builder.AppendLine(String.Empty);
+	        builder.Append("\tSize of extended data:             ");
+	        builder.Append(this.size);
+	        builder.AppendLine(String.Empty);
+
+	        desc = builder.ToString();
+	
+	        return desc;
+        }
+	    #endregion
+    }
+}
---
+++ Basic Structures/Win32/Audio/Wave Format Extensible.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text;
+
+namespace Bardez.Projects.BasicStructures.Win32.Audio
+{
+    /// <summary>Managed representation of Win32 WAVEFORMATEXTENSIBLE structure, inheriting the WaveFormatEx structure</summary>
+    public class WaveFormatExtensible : WaveFormatEx
+    {
+        #region Fields
+		/// <summary>Union of three fields here</summary>
+		protected UInt16 samples;
+
+		/// <summary>Positions of the audio channels</summary>
+		protected UInt32 channelMask;
+
+		/// <summary>Format identifier GUID</summary>
+		protected Guid subFormat;
+        #endregion
+
+
+        #region Properties
+		/// <summary>Base WAVEFORMATEX data</summary>
+		/// <remarks>Keeping with the base type paradigm in C/++</remarks>
+		public WaveFormatEx Format
+		{
+			get { return this as WaveFormatEx; }
+		}
+
+		/// <summary>Union of three fields here: wValidBitsPerSample, wSamplesPerBlock, and wReserved</summary>
+		public UInt16 Samples
+		{
+            get { return this.samples; }
+			set { this.samples = value; }
+		}
+					
+		/// <summary>Union of three fields here: wValidBitsPerSample, wSamplesPerBlock, and wReserved</summary>
+		public UInt16 ValidBitsPerSample
+		{
+            get { return this.samples; }
+			set { this.samples = value; }
+		}
+    
+		/// <summary>Union of three fields here: wValidBitsPerSample, wSamplesPerBlock, and wReserved</summary>
+		public UInt16 Reserved
+		{
+            get { return this.samples; }
+			set { this.samples = value; }
+		}
+
+		/// <summary>Positions of the audio channels</summary>
+		public UInt32 ChannelMask
+		{
+            get { return this.channelMask; }
+			set { this.channelMask = value; }
+		}
+
+		/// <summary>Format identifier GUID</summary>
+		public Guid SubFormat
+		{
+			get { return this.subFormat; }
+			set { this.subFormat = value; }
+		}
+        #endregion
+
+
+		#region Construction
+		/// <summary>Default constructor</summary>
+		public WaveFormatExtensible() { }
+        #endregion
+
+
+		#region Methods
+		/// <summary>Generates a descriptive string (to be displayed to the end user)</summary>
+		/// <returns>A String representing the WaveFormatEx Object contents</returns>
+		public override String ToDescriptionString()
+        {
+	        String desc = null;
+
+	        StringBuilder builder = new StringBuilder();
+
+	        //base type
+	        builder.Append(base.ToDescriptionString());
+
+	        //remainder data
+	        builder.Append("\tNumber of samples:                 ");
+	        builder.Append(this.samples);
+	        builder.AppendLine(String.Empty);
+	        builder.Append("\tAudio channel positions(s):        ");
+	        builder.Append(this.channelMask);
+	        builder.AppendLine(String.Empty);
+	        builder.Append("\tFormat GUID identifier:            ");
+	        builder.Append(this.subFormat.ToString());
+	        builder.AppendLine(String.Empty);
+
+	        desc = builder.ToString();
+	
+	        return desc;
+        }
+		#endregion
+
+    }
+}
