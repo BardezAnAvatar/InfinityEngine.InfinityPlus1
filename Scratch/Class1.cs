d20643dc127e7ff311e601bce9b484503b87ad18
Erik "Bardez" Frantz <bardez@bardez.net>
2012-12-15 16:33:48 +0000

    Added in a scratch project
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C465

---
+++ Scratch/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using OpenTK.Audio.OpenAL;
+
+namespace Scratch
+{
+    public class Class1
+    {
+    }
+}
