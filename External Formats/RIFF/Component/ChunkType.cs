35bb98b446faca688314af2dde45b0ec6c24b938
Erik "Bardez" Frantz <bardez@bardez.net>
2012-12-16 22:45:41 +0000

    Added in a coule of FourCC codes and changed the whitespace formatting a bit
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C476

--- External Formats/RIFF/Component/ChunkType.cs
+++ External Formats/RIFF/Component/ChunkType.cs
@@ -7,24 +7,26 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.External.RIFF.Component
     /// <remarks>I wrote with hex because the UInt32 represenation is backward from the actual value; if recompiling or a differnt endianness, invert the values</remarks>
     public enum ChunkType : uint /* UInt32 */
     {
-        RIFF = 0x46464952U,
-        LIST = 0x5453494CU,
-        JUNK = 0x4B4E554AU,
-        INFO = 0x4F464E49U,
-                         
-        WAVE = 0x45564157U,
-        fmt  = 0x20746D66U,
-        data = 0x61746164U,
-                         
-        AVI  = 0x20495641U,
-        hdrl = 0x6C726468U,
-        avih = 0x68697661U,
-        strl = 0x6C727473U,
-        strh = 0x68727473U,
-        strf = 0x66727473U,
-        strd = 0x64727473U,
-        movi = 0x69766F6DU,
-        rec  = 0x20636572U,
-        idx1 = 0x31786469U
+        RIFF    = 0x46464952U,  //Format/container FourCC
+        LIST    = 0x5453494CU,  //LIST of chunks
+        JUNK    = 0x4B4E554AU,  //Padding
+        INFO    = 0x4F464E49U,  //Informational
+
+        WAVE    = 0x45564157U,  //Wave container type
+        fmt     = 0x20746D66U,  //Wave WAVEFORMATEX
+        data    = 0x61746164U,  //generic data
+
+        AVI     = 0x20495641U,  //AVI container type
+        hdrl    = 0x6C726468U,
+        avih    = 0x68697661U,
+        strl    = 0x6C727473U,
+        strh    = 0x68727473U,
+        strf    = 0x66727473U,
+        strd    = 0x64727473U,
+        movi    = 0x69766F6DU,
+        rec     = 0x20636572U,
+        idx1    = 0x31786469U,
+        ISFT    = 0x54465349U,  //Software Identifier
+        fact    = 0x74636166U,  //No idea; found as a 4-byte field in a Tormented diss wav
     }
 }
