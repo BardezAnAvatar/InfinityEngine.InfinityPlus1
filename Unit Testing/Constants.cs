2121fb8e18f6eb4b2dc97b3723317590bd30bcc5
Erik "Bardez" Frantz <bardez@bardez.net>
2012-12-16 02:39:35 +0000

    Added test file and constants file
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C469

---
+++ Unit Testing/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bardez.Projects.InfinityPlus1.UnitTesting
+{
+    internal class Constants
+    {
+        /// <summary>String indicating an XML unit test data source</summary>
+        internal const String XmlDataSource = "Microsoft.VisualStudio.TestTools.DataSource.XML";
+    }
+}
