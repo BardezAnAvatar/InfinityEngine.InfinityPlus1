a1392dd730de659a502eb3bd94a3cd546a218545
Erik "Bardez" Frantz <bardez@bardez.net>
2012-12-17 00:34:43 +0000

    Changed Header to RootChunk, changed it to RiffSuperChunk; added Container Type and Container Type name properties
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C486

--- External Formats/RIFF/Component/RIFF File.cs
+++ External Formats/RIFF/Component/RIFF File.cs
@@ -10,34 +10,64 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.External.RIFF.Component
     /// <remarks>This should be a general RIFF file, not a specific one</remarks>
     public class RiffFile
     {
+        #region Fields
         /// <summary>Main Riff chunk</summary>
-        public RiffThroneChunk Header { get; set; }
+        public RiffSuperChunk RootChunk { get; set; }
+        #endregion
 
+
+        #region Properties
+        /// <summary>Four character code indicating the type of RIFF content within this super-chunk</summary>
+        public ChunkType ContainerType
+        {
+            get { return this.RootChunk.ContainerType; }
+        }
+
+        /// <summary>Four character code indicating the type of RIFF content within this super-chunk</summary>
+        public String ContainerTypeName
+        {
+            get { return RiffChunk.GetFourCCName((UInt32)(this.ContainerType)); ; }
+        }
+        #endregion
+
+
+        #region Read method(s)
         /// <summary>This public method reads file format from the input stream. Reads the whole structure.</summary>
         /// <param name="input">Input stream to read from</param>
         public void Read(Stream input)
         {
-            RiffChunk header = RiffChunkFactory.CreateChunk(input);
+            IRiffChunk header = RiffChunkFactory.CreateChunk(input);
 
             //Check that the first bits read was "RIFF"
-            if ( !(header is RiffThroneChunk))
-                throw new ApplicationException(String.Format("Expected to find a RiffThroneChunk type (for a RIFF fourCC) while expecting to read a RIFF file. Found \"{0}\" instead.", header== null ? "null" : header.GetType().Name));
+            if ( !(header is RiffSuperChunk))
+                throw new ApplicationException(String.Format("Expected to find a RiffSuperChunk type (for a RIFF fourCC) while expecting to read a RIFF file. Found \"{0}\" instead.", header== null ? "null" : header.GetType().Name));
 
-            this.Header = header as RiffThroneChunk;
-            this.Header.ReadChunkHeader();
-            this.Header.Read();
+            this.RootChunk = header as RiffSuperChunk;
+            this.RootChunk.ReadSubChunks();
         }
+        #endregion
 
+
+        #region ToString method(s)
         /// <summary>This method overrides the default ToString() method, printing the member data line by line</summary>
         /// <returns>A string containing the values and descriptions of all values in this class</returns>
         public override String ToString()
         {
             StringBuilder builder = new StringBuilder();
 
-            builder.Append(StringFormat.ToStringAlignment("Sample Data Length"));
-            builder.Append(this.Header.Data.Length);
+            this.WriteString(builder);
 
             return builder.ToString();
         }
+
+        /// <summary>This method prints a human-readable representation to the given StringBuilder</summary>
+        /// <param name="builder">StringBuilder to write to</param>
+        public void WriteString(StringBuilder builder)
+        {
+            builder.Append("RIFF File:");
+            StringFormat.ToStringAlignment("Chunks", builder);
+            StringFormat.IndentAllLines(this.RootChunk.ToString(), 1, builder);
+        }
+        #endregion
     }
 }
