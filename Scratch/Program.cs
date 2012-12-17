3210caa82518dd3a0979282895232009091095ed
Erik "Bardez" Frantz <bardez@bardez.net>
2012-12-17 00:58:36 +0000

    Added in a main program class
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C502

---
+++ Scratch/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Scratch
+{
+    /// <summary>Main program class/entry point object.</summary>
+    internal class Program
+    {
+        /// <summary>Driving main method application entry point</summary>
+        /// <param name="args">String Array of application arguments</param>
+        [MTAThread]
+        internal static void Main(String[] args)
+        {
+        }
+    }
+}
