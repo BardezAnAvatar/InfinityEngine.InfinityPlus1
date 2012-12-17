a83b965869c40ee9497509e664aaf3a79a162b43
Erik "Bardez" Frantz <bardez@bardez.net>
2012-12-17 00:46:19 +0000

    Changed behavior to create a base RiffChunk from the input stream, then to pass it into the extension chunk classes; added in Software Riff chunk case as well
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C493

--- External Formats/RIFF/Component/Riff Chunk Factory.cs
+++ External Formats/RIFF/Component/Riff Chunk Factory.cs
@@ -11,23 +11,25 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.External.RIFF.Component
         /// <summary>Generates a new RIFF chunk based off of the input chunk tyoe</summary>
         /// <param name="dataStream">Stream to read the chunk type from</param>
         /// <returns>A Constructed RIFF chunk based on the type of chunk detected</returns>
-        public static RiffChunk CreateChunk(Stream dataStream)
+        public static IRiffChunk CreateChunk(Stream dataStream)
         {
-            ChunkType chunkType = RiffChunk.ReadFourCC(dataStream);
-            RiffChunk chunk = null;
+            IRiffChunk chunk = new RiffChunk(dataStream);
 
-            switch (chunkType)
+            switch (chunk.ChunkId)
             {
                 case ChunkType.RIFF:
-                    chunk = new RiffThroneChunk(chunkType, dataStream);
+                case ChunkType.LIST:
+                    chunk = new RiffSuperChunk(chunk);
                     break;
                 case ChunkType.fmt:
-                    chunk = new WaveFormatChunk(chunkType, dataStream);
+                    chunk = new WaveFormatChunk(chunk);
+                    break;
+                case ChunkType.ISFT:
+                    chunk = new SoftwareRiffChunk(chunk);
                     break;
                 case ChunkType.data:    //originally had this as a Wave-specific type, but not appropriate unless interpresting data.
                 case ChunkType.JUNK:
                 default:
-                    chunk = new RiffChunk(chunkType, dataStream);
                     break;
             }
 
