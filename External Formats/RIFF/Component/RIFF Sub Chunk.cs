c0e6d7cb0201d891bd6bd5eec4871ef62f93198b
Erik "Bardez" Frantz <bardez@bardez.net>
2012-12-17 00:45:31 +0000

    Changed to a more recent .NET coding pattern with auto-properties; inherited from Riff Extension Base; implemented the ToString and WriteString pattern
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C492

--- External Formats/RIFF/Component/RIFF Sub Chunk.cs
+++ External Formats/RIFF/Component/RIFF Sub Chunk.cs
@@ -1,35 +1,55 @@
 ﻿using System;
+using System.Text;
+
+using Bardez.Projects.ReusableCode;
 
 namespace Bardez.Projects.InfinityPlus1.FileFormats.External.RIFF.Component
 {
     /// <summary>Simple structure that defines a chunk and the offset to that chunk in memory</summary>
     public class RiffSubChunk
     {
+        #region Fields
         /// <summary>Offset from start of file to position within data containing the sub-chunk</summary>
-        protected Int64 offset;
+        public Int64 Offset { get; set; }
 
         /// <summary>Riff chunk contained </summary>
-        protected RiffChunk chunk;
+        public IRiffChunk Chunk { get; set; }
+        #endregion
 
-        /// <summary>Offset from start of file to position within data containing the sub-chunk</summary>
-        public Int64 Offset
-        {
-            get { return this.offset; }
-            set { this.offset = value; }
-        }
 
-        /// <summary>Riff chunk contained </summary>
-        public RiffChunk Chunk
-        {
-            get { return this.chunk; }
-            set { this.chunk = value; }
-        }
-
+        #region Construction
         /// <summary>Default constuctor</summary>
         /// <param name="position">Offset within stream to start reading at</param>
         public RiffSubChunk(Int64 position)
         {
-            this.offset = position;
+            this.Offset = position;
         }
+        #endregion
+
+
+        #region ToString() method(s)
+        /// <summary>This method overrides the default ToString() method, printing a human-readable representation</summary>
+        /// <returns>A string containing the values and descriptions of all values in this class</returns>
+        public override String ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            this.WriteString(builder);
+
+            return builder.ToString();
+        }
+
+        /// <summary>This method prints a human-readable representation to the given StringBuilder</summary>
+        /// <param name="builder">StringBuilder to write to</param>
+        public void WriteString(StringBuilder builder)
+        {
+            StringFormat.ToStringAlignment("Type", builder);
+            builder.Append("Sub-chunk");
+            StringFormat.ToStringAlignment("Offset", builder);
+            builder.Append(this.Offset);
+            StringFormat.ToStringAlignment("Chunk", builder);
+            StringFormat.IndentAllLines(this.Chunk.ToString(), 1, builder);
+        }
+        #endregion
     }
 }
