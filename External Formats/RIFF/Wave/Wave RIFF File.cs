2c4a5ed4fc876936f78001430b0a963af86bb50e
Erik "Bardez" Frantz <bardez@bardez.net>
2012-12-17 04:03:28 +0000

    Removed stream constructor and added in ToString and WriteString pattern
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C505

--- External Formats/RIFF/Wave/Wave RIFF File.cs
+++ External Formats/RIFF/Wave/Wave RIFF File.cs
@@ -14,26 +14,23 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.External.RIFF.Wave
     /// </summary>
     public class WaveRiffFile : RiffFile
     {
-        #region Construction
-        /// <summary>PArtial definition constructor</summary>
-        /// <param name="input">Stream of which to take ownership of</param>
-        public WaveRiffFile(Stream input)
-        {
-        }
-        #endregion
-
         /// <summary>Returns a Win32 API WaveFormatEx object for submission to Windows APIs</summary>
         /// <returns>A Win32 API WaveFormatEx object with data from this RiffFile</returns>
         public WaveFormatEx GetWaveFormat()
         {
-            WaveFormatEx waveEx = new WaveFormatEx();
-            //WaveFormatExtensible waveExtensible = new WaveFormatExtensible();
-
             WaveFormatChunk format = (this.RootChunk.FindFirstSubChunk(ChunkType.fmt).Chunk as WaveFormatChunk);
-
             return format.GetWaveFormat();
         }
 
+        /// <summary>Gets binary sample data from Riff file</summary>
+        /// <returns>Byte array of sample data</returns>
+        public virtual Byte[] GetWaveData()
+        {
+            IRiffChunk wave = this.RootChunk.FindFirstSubChunk(ChunkType.data).Chunk;
+            return wave.Data;
+        }
+
+        #region ToString methods
         /// <summary>This method overrides the default ToString() method, printing the member data line by line</summary>
         /// <returns>A string containing the values and descriptions of all values in this class</returns>
         public override String ToString()
@@ -46,12 +43,13 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.External.RIFF.Wave
             return builder.ToString();
         }
 
-        /// <summary>Gets binary sample data from Riff file</summary>
-        /// <returns>Byte array of sample data</returns>
-        public virtual Byte[] GetWaveData()
+        /// <summary>This method prints a human-readable representation to the given StringBuilder</summary>
+        /// <param name="builder">StringBuilder to write to</param>
+        public void WriteString(StringBuilder builder)
         {
-            IRiffChunk wave = this.RootChunk.FindFirstSubChunk(ChunkType.data).Chunk;
-            return wave.Data;
+            WaveFormatEx format = this.GetWaveFormat();
+            builder
         }
+        #endregion
     }
 }
