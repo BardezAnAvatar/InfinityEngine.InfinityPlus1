c44e86d3bff3a7f09e9e692ca7e337c5a347afa3
Erik "Bardez" Frantz <bardez@bardez.net>
2012-12-17 00:41:49 +0000

    Added in a basic interface for RIFF chunks because I want a RIFF chunk to remember its start location and FourCC, and it seemed best to construct a chunk and then based on its values, pass it copy-wise into an expanded chunk. This would be an unnecessary copy and destroy, so I thought why not just have a local copy of the read chunk and access it; this breaks inheritance from RiffChunk, so I wanted to build an interface for the common properties and methods.
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C488

---
+++ External Formats/RIFF/Component/Interface RIFF Chunk.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Bardez.Projects.InfinityPlus1.FileFormats.External.RIFF.Component
+{
+    /// <summary>Interface that defines RIFF Chunk behavior and data exposure</summary>
+    public interface IRiffChunk
+    {
+        #region Properties
+        /// <summary>Leading four byte character code of the chunk</summary>
+        ChunkType ChunkId { get; }
+
+        /// <summary>Exposes the name of the chunk type. Preserves spaces in the name.</summary>
+        String ChunkName { get; }
+        
+        /// <summary>Unsigned 32-bit interger indicating the chunk size</summary>
+        UInt32 Size { get; }
+
+        /// <summary>Unsigned 32-bit interger indicating the chunk size</summary>
+        /// <remarks>
+        ///     The spec. defines this as a byte array. However, the problem with this is that, in the example of raw audio-video interleave,
+        ///     the data chunk may be so incredibly large as to surpass RAM, in which case reading the data is foolish. Instead, open the file
+        ///     and read the stream, seeking as appropriate, storing offsets and reading on-demand.
+        /// </remarks>
+        Byte[] Data { get; }
+
+        /// <summary>Unsigned offset to the data</summary>
+        Int64 DataOffset { get; }
+
+        /// <summary>Position within the stream where this chunk starts</summary>
+        Int64 ChunkOffset { get; }
+
+        /// <summary>Stream object containing the RIFF data to read from.</summary>
+        Stream DataStream { get; }
+        #endregion
+
+
+        #region Methods
+        /// <summary>This method prints a human-readable representation to the given StringBuilder</summary>
+        /// <param name="builder">StringBuilder to write to</param>
+        void WriteString(StringBuilder builder);
+        #endregion
+    }
+}
