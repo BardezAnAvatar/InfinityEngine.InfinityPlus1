9c8d748a1459e89307b269a98f5040a35fc9f11b
Erik "Bardez" Frantz <bardez@bardez.net>
2012-12-27 08:27:26 +0000

    Added in the current pixel conversion code into a LibAV wrapper IPixelConverter class specific for LibAV code
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C542

---
+++ Interface Implementation/LibAV Wrapper/LibAV Pixel Converter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+
+using Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video.Pixels;
+using Bardez.Projects.InfinityPlus1.FileFormats.MediaBase.Video.Pixels.Enums;
+using Bardez.Projects.MultiMedia.LibAV;
+
+namespace Bardez.Projects.MultiMedia.LibAV.Wrapper
+{
+    public class LibAVPixelConverter : IPixelConverter
+    {
+        /// <summary>Converts data from the current format to another specified format</summary>
+        /// <param name="data">Decompressed data to convert</param>
+        /// <param name="sourceFormat">Format to convert from</param>
+        /// <param name="destinationFormat">Format to convert to</param>
+        /// <param name="horizontalPacking">Row packing</param>
+        /// <param name="verticalPacking">Row count to align to</param>
+        /// <param name="sourceWidth">Indicates the source width of the image data</param>
+        /// <param name="sourceHeight">Indicates the source height of the image data</param>
+        /// <param name="decodedBitDepth">The bits per pixel once decoded</param>
+        /// <returns>New byte data</returns>
+        public MemoryStream ConvertData(MemoryStream data, PixelFormat sourceFormat, PixelFormat destinationFormat, Int32 horizontalPacking, Int32 verticalPacking, Int32 sourceWidth, Int32 sourceHeight, Int32 decodedBitDepth)
+        {
+            MemoryStream result = null;
+
+            //translate pixel formats
+            LibAVPixelFormat sourceFormatLibAV = sourceFormat.ToLibAVPixelFormat();
+            LibAVPixelFormat destFormatLibAV = destinationFormat.ToLibAVPixelFormat();
+
+            using (SWScale scale = new SWScale(
+                        LibAVPictureDetail.Build(sourceWidth, sourceHeight, sourceFormatLibAV),
+                        LibAVPictureDetail.Build(sourceWidth, sourceHeight, destFormatLibAV),
+                        ResizeOptions.Lanczos))
+            {
+                MemoryStream outStream = null;
+                scale.Transform(data, out outStream);
+                result = outStream;
+            }
+
+            return result;
+        }
+    }
+}
