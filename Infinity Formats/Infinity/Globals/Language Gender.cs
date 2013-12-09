9366387eaa20aeeded9d17a437224f1af0a80d5a
Erik "Bardez" Frantz <bardez@bardez.net>
2013-12-09 07:57:45 +0000

    Added in a enum to specify a dialog.tlk gender
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C804

---
+++ Infinity Formats/Infinity/Globals/Language Gender.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Globals
+{
+    /// <summary>Specifies the gender model for languages</summary>
+    public enum LanguageGender
+    {
+        Masculine,
+        Feminine,
+    }
+}
