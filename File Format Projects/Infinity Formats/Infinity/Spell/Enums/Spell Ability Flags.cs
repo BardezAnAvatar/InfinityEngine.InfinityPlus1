a7b4aae5ebc8723842a72c1430a603136d5747f2
Erik "Bardez" Frantz <bardez@bardez.net>
2014-01-02 04:46:06 +0000

    Added spell ability flags, which was a TODO
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C876

---
+++ File Format Projects/Infinity Formats/Infinity/Spell/Enums/Spell Ability Flags.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Spell.Enums
+{
+    /// <summary>Flag describing spell hostility and possibly other values as well</summary>
+    /// <remarks>A Hostile spell is one that does not have the friendly bit set</remarks>
+    [Flags]
+    public enum SpellAbilityFlags : byte /* Byte */
+    {
+        /// <summary>A non-offensive spell</summary>
+        Friendly    = 0x04,
+    }
+}
