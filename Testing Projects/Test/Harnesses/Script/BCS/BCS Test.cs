4fb0c47c5d1d8ca28ba639c43246b70e6f986aa0
Erik "Bardez" Frantz <bardez@bardez.net>
2014-01-02 04:16:40 +0000

    Added a script test harness
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C847

---
+++ Testing Projects/Test/Harnesses/Script/BCS/BCS Test.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+
+using Bardez.Projects.Configuration;
+using Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Script.Components;
+using Bardez.Projects.InfinityPlus1.Test;
+using Bardez.Projects.Utility;
+
+namespace Bardez.Projects.InfinityPlus1.Test.Harnesses.Script.BCS
+{
+    /// <summary>A testing harness for *.BCS files</summary>
+    public class BioWareCompiledScriptTest : FileTesterBase
+    {
+        #region Constants
+        /// <summary>Constant key to look up in app.config</summary>
+        protected const String configKey = "Test.BCS.Path";
+        #endregion
+
+
+        #region Fields
+        /// <summary>Format instance to test</summary>
+        protected BiowareScript script { get; set; }
+        #endregion
+
+
+        #region Construction
+        /// <summary>Default constructor</summary>
+        public BioWareCompiledScriptTest()
+        {
+            this.InitializeInstance();
+        }
+        #endregion
+
+
+        #region Harness methods
+        /// <summary>Initializes the test class data</summary>
+        /// <param name="sender">Object sending/raising the request</param>
+        /// <param name="e">Specific initialization event parameters</param>
+        protected override void InitializeTestData(Object sender, EventArgs e)
+        {
+            this.FilePaths = ConfigurationHandlerMulti.GetSettingValues(BioWareCompiledScriptTest.configKey);
+        }
+
+        /// <summary>Event to raise for testing instance(s)</summary>
+        /// <param name="sender">Object sending/raising the request</param>
+        /// <param name="testArgs">Arguments containing the item to test (usually a file path)</param>
+        protected override void TestCase(Object sender, TestEventArgs testArgs)
+        {
+            using (FileStream stream = new FileStream(testArgs.Path, FileMode.Open, FileAccess.Read))
+            {
+                this.script = new BiowareScript();
+                this.DoPostMessage(new MessageEventArgs("Reading BCS file...", "Reading", testArgs.Path));
+                this.script.Read(stream);
+                this.DoPostMessage(new MessageEventArgs(this.script.ToString(), "Output", testArgs.Path));
+            }
+
+            using (FileStream dest = new FileStream(testArgs.Path + ".rewrite", FileMode.Create, FileAccess.Write))
+                this.TestWrite(dest);
+        }
+
+        /// <summary>Writes the data structure back out to a destination stream</summary>
+        /// <param name="destination">Stream to write output to</param>
+        protected virtual void TestWrite(Stream destination)
+        {
+            this.script.Write(destination);
+        }
+        #endregion
+    }
+}
