4c29627d36da0bd569c74c3621bf82f81b44f640
Erik "Bardez" Frantz <bardez@bardez.net>
2014-01-02 04:34:29 +0000

    Added summaries to each member; refactored protected/public properties; changed abilitiesArray to an IList
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C865

--- File Format Projects/Infinity Formats/Infinity/Common/ItmSpl/ItemSpell.cs
+++ File Format Projects/Infinity Formats/Infinity/Common/ItmSpl/ItemSpell.cs
@@ -9,47 +9,54 @@ using Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Effect.Effect1;
 
 namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Common.ItmSpl
 {
+    /// <summary>Common elements between an item and a spell</summary>
     public abstract class ItemSpell : IInfinityFormat
     {
-        #region Members
-        protected ItemSpellHeader1 header;
-        protected List<Effect1> effects;
+        #region Fields
+        /// <summary>The header of this item or spell</summary>
+        protected ItemSpellHeader header;
+
+        /// <summary>Collection of effects related to this item or spell</summary>
+        public List<Effect1> Effects { get; set; }
         #endregion
 
-        #region Properties
-        public List<Effect1> Effects
-        {
-            get { return this.effects; }
-            set { this.effects = value; }
-        }
 
+        #region Properties
+        /// <summary>Descriptive headline for the item or spell</summary>
         protected abstract String Headline
         {
             get;
         }
 
+        /// <summary>Size of the item's or spell's header</summary>
         protected abstract UInt32 HeaderSize
         {
             get;
         }
 
+        /// <summary>Size of this item's or spell's abilities</summary>
         protected abstract UInt32 AbilitySize
         {
             get;
         }
 
-        protected abstract ItemSpellAbility[] abilitiesArray
+        /// <summary>Collection of abilities associated with this item or spell</summary>
+        protected abstract IList<ItemSpellAbility> Abilities
         {
             get;
         }
         #endregion
 
+
+        #region Construction
         /// <summary>Instantiates reference types</summary>
         public abstract void Initialize();
 
         /// <summary>Instantiates a new header</summary>
         protected abstract void InstantiateHeader();
-        
+        #endregion
+
+
         #region IO method implemetations
         /// <summary>This public method reads file format from the output stream. Reads the whole structure.</summary>
         /// <param name="input">Stream to read from</param>
@@ -89,7 +96,7 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Common.ItmSpl
             {
                 Effect1 effect = new Effect1();
                 effect.Read(input);
-                this.effects.Add(effect);
+                this.Effects.Add(effect);
             }
         }
 
@@ -106,15 +113,17 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Common.ItmSpl
             this.header.Write(output);
 
             output.Seek(this.header.OffsetAbilities, SeekOrigin.Begin);
-            foreach (ItemSpellAbility ability in this.abilitiesArray)
+            foreach (ItemSpellAbility ability in this.Abilities)
                 ability.Write(output);
 
             output.Seek(this.header.OffsetAbilityEffects, SeekOrigin.Begin);
-            foreach (Effect1 effect in this.effects)
+            foreach (Effect1 effect in this.Effects)
                 effect.Write(output);
         }
         #endregion
 
+
+        #region ToString override(s)
         /// <summary>This method overrides the default ToString() method, printing the member data line by line</summary>
         /// <returns>A string containing the values and descriptions of all values in this class</returns>
         public override String ToString()
@@ -123,14 +132,16 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Common.ItmSpl
             builder.Append(this.Headline);
             builder.Append(this.header.ToString());
 
-            for (Int32 i = 0; i < this.abilitiesArray.Length; ++i)
-                builder.Append(abilitiesArray[i].ToString(i + 1));
+            for (Int32 i = 0; i < this.Abilities.Count; ++i)
+                builder.Append(Abilities[i].ToString(i + 1));
 
-            for (Int32 i = 0; i < this.effects.Count; ++i)
-                builder.Append(effects[i].ToString(i + 1));
+            for (Int32 i = 0; i < this.Effects.Count; ++i)
+                builder.Append(Effects[i].ToString(i + 1));
 
             return builder.ToString();
         }
+        #endregion
+
 
         /// <summary>Maintains mimimal data integrity by not lying to the output data file.</summary>
         protected void MaintainMinimalDataIntegrity()
@@ -139,20 +150,20 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Common.ItmSpl
             Int32 effectSize = Effect1.StructSize;
             UInt32 abilitySize = this.AbilitySize;
 
-            this.header.CountAbilities = Convert.ToUInt16(this.abilitiesArray.Length);
+            this.header.CountAbilities = Convert.ToUInt16(this.Abilities.Count);
 
             if (this.header.OffsetAbilities < headerSize)
                 this.header.OffsetAbilities = headerSize;
 
             //make sure there is space for the effects. Yes, they *can* be in between the header and the abilities, so poo.
-            Int64 size = this.effects.Count * effectSize;
+            Int64 size = this.Effects.Count * effectSize;
             if (    //(
                         //!((this.header.OffsetAbilityEffects + size) < this.header.OffsetAbilities)
                         //&& !(this.header.OffsetAbilityEffects > this.header.OffsetAbilities && this.header.OffsetAbilityEffects < (this.header.OffsetAbilities + (this.abilitiesArray.Length * abilitySize)))
                     //)
                     //...wow that was bad code
                     (this.header.OffsetAbilityEffects < this.header.OffsetAbilities && (this.header.OffsetAbilityEffects + size) >= this.header.OffsetAbilities) //it starts before and spills into
-                    || (this.header.OffsetAbilityEffects >= this.header.OffsetAbilities && (this.header.OffsetAbilityEffects < (this.header.OffsetAbilities + (this.abilitiesArray.Length * abilitySize))))            //it starts after abilities, but before the end of abilities
+                    || (this.header.OffsetAbilityEffects >= this.header.OffsetAbilities && (this.header.OffsetAbilityEffects < (this.header.OffsetAbilities + (this.Abilities.Count * abilitySize))))            //it starts after abilities, but before the end of abilities
                 )
                 this.header.OffsetAbilityEffects = headerSize + (abilitySize * this.header.CountAbilities);
         }
@@ -163,7 +174,7 @@ namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Common.ItmSpl
         {
             Int32 count = this.header.CountEquippedEffects;
 
-            foreach (ItemSpellAbility ability in this.abilitiesArray)
+            foreach (ItemSpellAbility ability in this.Abilities)
                 count += ability.FeatureCount;
 
             return count;
