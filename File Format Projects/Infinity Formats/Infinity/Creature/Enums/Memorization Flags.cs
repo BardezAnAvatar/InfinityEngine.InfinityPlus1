48cc3866321cf8209b07957818782eb0d6364919
Erik "Bardez" Frantz <bardez@bardez.net>
2014-01-02 04:31:29 +0000

    Added memotization flags, since that was part of the refactor
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C862

---
+++ File Format Projects/Infinity Formats/Infinity/Creature/Enums/Memorization Flags.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Creature.Enums
+{
+    /// <summary>Flags indicating the various memorization settings</summary>
+    [Flags]
+    public enum MemorizationFlags : uint /* UInt32 */
+    {
+        Memorized   = 1U,
+        Disabled    = 2U,
+    }
+}
