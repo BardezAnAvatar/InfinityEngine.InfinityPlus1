148d2f56b0173905005aefc39ace4ac303021878
Erik "Bardez" Frantz <bardez@bardez.net>
2014-01-02 04:50:07 +0000

    Added class summary; reduced the field size to Byte, per BG2 code and after realizing mismatch with 2DA values
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C879

--- File Format Projects/Infinity Formats/Infinity/Spell/Enums/Spell School.cs
+++ File Format Projects/Infinity Formats/Infinity/Spell/Enums/Spell School.cs
@@ -2,28 +2,23 @@
 
 namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Spell.Enums
 {
-    /// <summary>
-    /// 
-    /// </summary>
+    /// <summary>The magic school associated with a spell</summary>
     /// <remarks>
     ///     These values are in the ROW ORDER of the values foundin BG2's mschool.2da, and supposedly IWD's school.2da,
-    ///     but I cannot find it. These rows have String References to their names.
-    ///     
-    ///     ... I suspect, however, that the leading byte is part  this field, and the unused trailing byte is a
-    ///     similar leading byte of the next field.
+    ///     but I cannot find it. These rows have String References to their names. Amusingly, the default value is a 64-bit integer (2^32), not -1 (2^32 - 1)
     /// </remarks>
-    public enum SpellSchool : ushort /* UInt16 */
+    public enum SpellSchool : byte /* Byte */
     {
-        None            = 0x0000,
-        Abjurer         = 0x0100,
-        Conjurer        = 0x0200,
-        Diviner         = 0x0300,
-        Enchanter       = 0x0400,
-        Illusionist     = 0x0500,
-        Invoker         = 0x0600,
-        Necromancer     = 0x0700,
-        Transmuter      = 0x0800,
-        Generalist      = 0x0900,
-        //, WildMage    = 0x0A00
+        None            = 0x00,
+        Abjurer         = 0x01,
+        Conjurer        = 0x02,
+        Diviner         = 0x03,
+        Enchanter       = 0x04,
+        Illusionist     = 0x05,
+        Invoker         = 0x06,
+        Necromancer     = 0x07,
+        Transmuter      = 0x08,
+        Generalist      = 0x09,
+        //, WildMage    = 0x0A
     }
 }
