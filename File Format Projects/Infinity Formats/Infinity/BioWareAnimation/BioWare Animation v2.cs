2468469f38c933ef915db25b3b97d9415bf5d20f
Erik "Bardez" Frantz <bardez@bardez.net>
2014-01-14 14:10:02 +0000

    Added in preliminary BAM v2 code
    git-tfs-id: [http://bardezserver2:8080/tfs/DefaultCollection]$/Infinity Engine;C926

---
+++ File Format Projects/Infinity Formats/Infinity/BioWareAnimation/BioWare Animation v2.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using Bardez.Projects.InfinityPlus1.FileFormats.External.ImaginationTechnologies.PowerVR;
+using Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Base;
+using Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Common;
+using Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Common.BamMos;
+using Bardez.Projects.InfinityPlus1.FileFormats.Infinity.BioWareAnimation.Component;
+using Bardez.Projects.Multimedia.MediaBase.Data.Pixels;
+using Bardez.Projects.Multimedia.MediaBase.Data.Pixels.Enums;
+using Bardez.Projects.Multimedia.MediaBase.Frame.Image;
+using Bardez.Projects.ReusableCode;
+
+namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.BioWareAnimation
+{
+    /// <summary>Wrapper class for a BAM v2 file</summary>
+    public class BioWareAnimation_v2 : IInfinityFormat, IImageSet, IAnimation
+    {
+        #region Fields
+        /// <summary>Represents the BAM file header</summary>
+        public BamHeader_v2 Header { get; set; }
+
+        /// <summary>Collection of frame entries</summary>
+        public List<FrameEntry_v2> FrameEntries { get; set; }
+
+        /// <summary>Collection of animation entries</summary>
+        public List<AnimationEntry> Animations { get; set; }
+
+        /// <summary>Collection of the BAM file's data blocks</summary>
+        public List<DataBlock> DataBlocks { get; set; }
+
+        /// <summary>Collection of related files to this BAM, keyed by the data blocks' RelatedFileName</summary>
+        public Dictionary<String, PowerVrCompressed> RelatedFiles { get; set; }
+        #endregion
+
+
+        #region Properties
+        /// <summary>Exposes the count of frame indeces in this BAM by looping through animations</summary>
+        public Int32 CountFrameIndeces
+        {
+            get
+            {
+                Int32 count = 0;
+                foreach (AnimationEntry animation in this.Animations)
+                    count += animation.FrameCount;
+
+                return count;
+            }
+        }
+
+        /// <summary>Exposes the minimum CenterX value found</summary>
+        public Int32 MinCenterX
+        {
+            get
+            {
+                Int32 centerX = Int32.MaxValue;
+
+                foreach (FrameEntry_v2 frame in this.FrameEntries)
+                {
+                    if (frame.CenterX < centerX)
+                        centerX = frame.CenterX;
+                }
+
+                if (centerX == Int32.MaxValue)
+                    centerX = 0;
+
+                return centerX;
+            }
+        }
+
+        /// <summary>Exposes the minimum CenterY value found</summary>
+        public Int32 MinCenterY
+        {
+            get
+            {
+                Int32 centerY = Int32.MaxValue;
+
+                foreach (FrameEntry_v2 frame in this.FrameEntries)
+                {
+                    if (frame.CenterY < centerY)
+                        centerY = frame.CenterY;
+                }
+
+                if (centerY == Int32.MaxValue)
+                    centerY = 0;
+
+                return centerY;
+            }
+        }
+
+        /// <summary>Exposes the maximum CenterX value found</summary>
+        public Int32 MaxCenterX
+        {
+            get
+            {
+                Int32 centerX = Int32.MinValue;
+
+                foreach (FrameEntry_v2 frame in this.FrameEntries)
+                {
+                    if (frame.CenterX > centerX)
+                        centerX = frame.CenterX;
+                }
+
+                if (centerX == Int32.MinValue)
+                    centerX = 0;
+
+                return centerX;
+            }
+        }
+
+        /// <summary>Exposes the maximum CenterY value found</summary>
+        public Int32 MaxCenterY
+        {
+            get
+            {
+                Int32 centerY = Int32.MinValue;
+
+                foreach (FrameEntry_v2 frame in this.FrameEntries)
+                {
+                    if (frame.CenterY > centerY)
+                        centerY = frame.CenterY;
+                }
+
+                if (centerY == Int32.MinValue)
+                    centerY = 0;
+
+                return centerY;
+            }
+        }
+        #endregion
+
+
+        #region Construction
+        /// <summary>Instantiates reference types</summary>
+        public void Initialize()
+        {
+            this.Header = new BamHeader_v2();
+            this.FrameEntries = new List<FrameEntry_v2>();
+            this.Animations = new List<AnimationEntry>();
+            this.DataBlocks = new List<DataBlock>();
+            this.RelatedFiles = null;
+        }
+        #endregion
+
+
+        #region IO method implemetations
+        #region Read Methods
+        /// <summary>This public method reads file format from the input stream. Reads the whole structure.</summary>
+        /// <param name="input">Input stream to read from</param>
+        public virtual void Read(Stream input)
+        {
+            this.ReadBody(input);
+        }
+
+        /// <summary>This public method reads file format data structure from the input stream.</summary>
+        /// <param name="input">Stream to read from.</param>
+        /// <param name="fullRead">Boolean indicating whether to read the full stream or just everything after the identifying signature (and possibly version)</param>
+        public virtual void Read(Stream input, Boolean fullRead)
+        {
+            if (fullRead)
+                this.Read(input);
+            else
+            {
+                this.Header = new BamHeader_v2();
+                this.Header.Read(input, false);
+            }
+        }
+
+        /// <summary>This public method reads file format data structure from the input stream, after the signature has already been read.</summary>
+        /// <param name="input">Stream to read from</param>
+        public virtual void ReadBody(Stream input)
+        {
+            this.Initialize();
+
+            this.Header.Read(input);
+            this.ReadFrameEntries(input);
+            this.ReadAnimationEntries(input);
+            this.ReadDataBlocks(input);
+        }
+
+        /// <summary>Reads frame entries from the input Stream</summary>
+        /// <param name="input">Stream to read from</param>
+        public virtual void ReadFrameEntries(Stream input)
+        {
+            ReusableIO.SeekIfAble(input, this.Header.OffsetFrameEntries);
+
+            for (Int32 i = 0; i < this.Header.CountFrames; ++i)
+            {
+                FrameEntry_v2 frame = new FrameEntry_v2();
+                frame.Read(input);
+                this.FrameEntries.Add(frame);
+            }
+        }
+
+        /// <summary>Reads animation entries from the input Stream</summary>
+        /// <param name="input">Stream to read from</param>
+        public virtual void ReadAnimationEntries(Stream input)
+        {
+            ReusableIO.SeekIfAble(input, this.Header.OffsetAnimationEntries);
+            for (Int32 i = 0; i < this.Header.CountAnimations; ++i)
+            {
+                AnimationEntry animation = new AnimationEntry();
+                animation.Read(input);
+                this.Animations.Add(animation);
+            }
+        }
+
+        /// <summary>Reads data blocks form the input Stream</summary>
+        /// <param name="input">Stream to read from</param>
+        public virtual void ReadDataBlocks(Stream input)
+        {
+            ReusableIO.SeekIfAble(input, this.Header.OffsetDataBlocks);
+            for (Int32 i = 0; i < this.Header.CountDataBlocks; ++i)
+            {
+                DataBlock block = new DataBlock();
+                block.Read(input);
+                this.DataBlocks.Add(block);
+            }
+        }
+        #endregion
+
+
+        #region Write Methods
+        /// <summary>This public method writes the file format to the output stream.</summary>
+        /// <param name="output">Stream to write to</param>
+        public virtual void Write(Stream output)
+        {
+            this.Header.Write(output);
+            this.WriteFrameEntries(output);
+            this.WriteAnimationEntries(output);
+            this.WriteDataBlocks(output);
+        }
+
+        /// <summary>Writes the frame entries to the output Stream</summary>
+        /// <param name="output">Stream to write to</param>
+        public virtual void WriteFrameEntries(Stream output)
+        {
+            ReusableIO.SeekIfAble(output, this.Header.OffsetFrameEntries);
+
+            foreach (FrameEntry_v2 frame in this.FrameEntries)
+                frame.Write(output);
+        }
+
+        /// <summary>Writes the animation entries to the output Stream</summary>
+        /// <param name="output">Stream to write to</param>
+        public virtual void WriteAnimationEntries(Stream output)
+        {
+            foreach (AnimationEntry animation in this.Animations)
+                animation.Write(output);
+        }
+
+        /// <summary>Writes the BAM data blocks to the output Stream</summary>
+        /// <param name="output">Stream to write to</param>
+        public virtual void WriteDataBlocks(Stream output)
+        {
+            ReusableIO.SeekIfAble(output, this.Header.OffsetDataBlocks);
+            foreach (DataBlock block in this.DataBlocks)
+                block.Write(output);
+        }
+        #endregion
+        #endregion
+
+
+        #region IImageSet Method(s)
+        /// <summary>Returns the frame with the associated index in the tileset</summary>
+        /// <param name="index">Index of the frame to retrieve</param>
+        /// <returns>The frane at the associated index</returns>
+        public IMultimediaImageFrame GetFrame(Int32 index)
+        {
+            FrameEntry_v2 frame = this.FrameEntries[index];
+
+            //TODO: compose an image together from data blocks
+
+            Byte[] data = null;
+
+            Int64 oX = this.FrameEntries[index].RenderOriginX;
+            Int64 oY = this.FrameEntries[index].RenderOriginY;
+            PixelData pd = new PixelData(data, ScanLineOrder.TopDown, PixelFormat.RGBA_B8G8R8A8, frame.Height, frame.Width, 0, 0, 32, oX, oY);
+            IMultimediaImageFrame getFrame = new BasicImageFrame(pd);
+
+            return getFrame;
+        }
+
+        /// <summary>IImageSet property that exposes the frame count</summary>
+        public Int64 FrameCount
+        {
+            get { return this.FrameEntries.Count; }
+        }
+        #endregion
+
+
+        #region IAnimation Methods
+        /// <summary>Returns an IList containing an IList of indeces meant to be used in conjunction with <see cref="IImageSet.GetFrame(Int32)"/></summary>
+        /// <returns>An IList with items inside being IList of Int32 indeces to the frames returned from <see cref="IImageSet.GetFrame(Int32)"/> using the same index key</returns>
+        public IList<IList<Int32>> GetFrameAnimations()
+        {
+            IList<IList<Int32>> animations = new List<IList<Int32>>();
+
+            foreach (AnimationEntry animation in this.Animations)
+            {
+                List<Int32> animationFrames = new List<Int32>();
+                for (Int32 index = animation.StartFrame; index < (animation.StartFrame + animation.FrameCount); ++index)
+                    animationFrames.Add(index);
+
+                animations.Add(animationFrames);
+            }
+
+            return animations;
+        }
+        #endregion
+    }
+}
---
+++ File Format Projects/Infinity Formats/Infinity/BioWareAnimation/Component/BAM 2 Header.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+using Bardez.Projects.InfinityPlus1.FileFormats.Basic;
+using Bardez.Projects.InfinityPlus1.FileFormats.Infinity.Base;
+using Bardez.Projects.ReusableCode;
+
+namespace Bardez.Projects.InfinityPlus1.FileFormats.Infinity.BioWareAnimation.Component
+{
+    /// <summary>Represents a BAM v2 file's header</summary>
+    public class BamHeader_v2 : InfinityFormat
+    {
+        #region Constants
+        /// <summary>Size of this file structure on disk</summary>
+        public const Int32 StructSize = 32;
+        #endregion
+
+
+        #region Fields
+        /// <summary>Count of individual frames within this BAM file</summary>
+        public UInt32 CountFrames { get; set; }
+
+        /// <summary>Count of animations within this BAM file</summary>
+        /// <remarks>Animations in NI, Cycles in IESDP</remarks>
+        public UInt32 CountAnimations { get; set; }
+
+        /// <summary>Count of blocks</summary>
+        public Int32 CountDataBlocks { get; set; }
+
+        /// <summary>Offset to the frame entry structures</summary>
+        public UInt32 OffsetFrameEntries { get; set; }
+
+        /// <summary>Offset to the animation entry structures</summary>
+        public UInt32 OffsetAnimationEntries { get; set; }
+
+        /// <summary>Offset to the PVRZ data blocks</summary>
+        public UInt32 OffsetDataBlocks { get; set; }
+        #endregion
+
+
+        #region Construction
+        /// <summary>Instantiates reference types</summary>
+        public override void Initialize() { }
+        #endregion
+
+
+        #region IO method implemetations
+        /// <summary>This public method reads file format from the input stream, after the header has already been read.</summary>
+        /// <param name="input">Input stream to read from</param>
+        public override void ReadBody(Stream input)
+        {
+            Byte[] buffer = ReusableIO.BinaryRead(input, BamHeader.StructSize - 8);   //header buffer
+
+            this.CountFrames = ReusableIO.ReadUInt32FromArray(buffer, 0);
+            this.CountAnimations = ReusableIO.ReadUInt32FromArray(buffer, 4);
+            this.CountDataBlocks = ReusableIO.ReadInt32FromArray(buffer, 8);
+            this.OffsetFrameEntries = ReusableIO.ReadUInt32FromArray(buffer, 12);
+            this.OffsetAnimationEntries = ReusableIO.ReadUInt32FromArray(buffer, 16);
+            this.OffsetDataBlocks = ReusableIO.ReadUInt32FromArray(buffer, 20);
+        }
+
+        /// <summary>This public method writes the header to an output stream</summary>
+        /// <param name="output">Stream object into which to write to</param>
+        public override void Write(Stream output)
+        {
+            ReusableIO.WriteStringToStream(this.signature, output, CultureConstants.CultureCodeEnglish, false, 4);
+            ReusableIO.WriteStringToStream(this.version, output, CultureConstants.CultureCodeEnglish, false, 4);
+            ReusableIO.WriteUInt32ToStream(this.CountFrames, output);
+            ReusableIO.WriteUInt32ToStream(this.CountAnimations, output);
+            ReusableIO.WriteInt32ToStream(this.CountDataBlocks, output);
+            ReusableIO.WriteUInt32ToStream(this.OffsetFrameEntries, output);
+            ReusableIO.WriteUInt32ToStream(this.OffsetAnimationEntries, output);
+            ReusableIO.WriteUInt32ToStream(this.OffsetDataBlocks, output);
+        }
+        #endregion
+
+
+        #region Public Methods
+        /// <summary>This method overrides the default ToString() method, printing the member data line by line</summary>
+        /// <returns>A string containing the values and descriptions of all values in this class</returns>
+        /// <remarks>TIS header signatures can be null-terminated rather than have whitespace</remarks>
+        public override String ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append("BAM v2 Header:");
+            builder.Append(StringFormat.ToStringAlignment("Signature"));
+            builder.Append(String.Format("'{0}'", this.signature));
+            builder.Append(StringFormat.ToStringAlignment("Version"));
+            builder.Append(String.Format("'{0}'", this.version));
+            builder.Append(StringFormat.ToStringAlignment("Frame Count"));
+            builder.Append(this.CountFrames);
+            builder.Append(StringFormat.ToStringAlignment("Animation Count"));
+            builder.Append(this.CountAnimations);
+            builder.Append(StringFormat.ToStringAlignment("Data Block Count"));
+            builder.Append(this.CountDataBlocks);
+            builder.Append(StringFormat.ToStringAlignment("Frame Entries Offset"));
+            builder.Append(this.OffsetFrameEntries);
+            builder.Append(StringFormat.ToStringAlignment("Animation Entries Offset"));
+            builder.Append(this.OffsetAnimationEntries);
+            builder.Append(StringFormat.ToStringAlignment("Data Block Offset"));
+            builder.Append(this.OffsetDataBlocks);
+
+            return builder.ToString();
+        }
+        #endregion
+
+
+        #region Equality
+        /// <summary>Overridden (value) equality method</summary>
+        /// <param name="obj">Object to compare to</param>
+        /// <returns>Boolean indicating equality</returns>
+        public override Boolean Equals(Object obj)
+        {
+            Boolean equal = false;  //assume the worst
+
+            try
+            {
+                if (obj != null && obj is BamHeader_v2)
+                {
+                    BamHeader_v2 compare = obj as BamHeader_v2;
+
+                    Boolean structureEquality = (this.CountFrames == compare.CountFrames);
+                    structureEquality &= (this.CountAnimations == compare.CountAnimations);
+                    structureEquality &= (this.CountDataBlocks == compare.CountDataBlocks);
+
+                    //offsets are unimportant when it comes to data value equivalence/equality
+                    equal = structureEquality;
+                }
+            }
+            catch { equal = false; }    //per MSDN, must not throw exceptions
+
+            return equal;
+        }
+
+        /// <summary>Override of GetHashCode</summary>
+        /// <returns>Computed hash</returns>
+        public override Int32 GetHashCode()
+        {
+            Int32 hash = base.GetHashCode();
+            hash ^= this.CountFrames.GetHashCode();
+            hash ^= this.CountAnimations.GetHashCode();
+            hash ^= this.CountDataBlocks.GetHashCode();
+            //offsets are unimportant when it comes to data value equivalence/equality
+
+            return hash;
+        }
+        #endregion
+    }
+}
